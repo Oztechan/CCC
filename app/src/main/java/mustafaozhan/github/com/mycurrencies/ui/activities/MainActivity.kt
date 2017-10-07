1bac78884768031757397f8c890570b952b9ef7c
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-10-07 18:49:34 +0300

    Rx support

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/MainActivity.kt
@@ -2,7 +2,11 @@ package mustafaozhan.github.com.mycurrencies.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
 import android.view.Menu
 import android.view.MenuItem
 import mustafaozhan.github.com.mycurrencies.R
@@ -13,6 +17,7 @@ import mustafaozhan.github.com.mycurrencies.rest.ApiInterface
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,23 +30,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun set() {
-        mSpinner.setOnItemSelectedListener { view, position, id, item ->
-            val apiService = ApiClient.get().create(ApiInterface::class.java)
-            val myCall = apiService.getByBase(item.toString())
-            myCall.enqueue(object : Callback<ResponseAll> {
-                override fun onResponse(call: Call<ResponseAll>?, response: Response<ResponseAll>?) {
-                    var value = 0.0
-                    if (eTxt.text.toString() != "")
-                        value = java.lang.Double.parseDouble(eTxt.text.toString())
-                    Log.e("${eTxt.text} $item ", "${(response!!.body()!!.rates!!.tRY!! * value)} Turkish Lira")
-                }
 
-                override fun onFailure(call: Call<ResponseAll>?, t: Throwable?) {
+        val apiService = ApiClient.get().create(ApiInterface::class.java)
 
-                }
 
+
+
+        Observable.create(Observable.OnSubscribe<String> { subscriber ->
+            eTxt.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) = Unit
+
+                override fun beforeTextChanged(s: CharSequence?,
+                                               start: Int, count: Int,
+                                               after: Int) = Unit
+
+                override fun onTextChanged(s: CharSequence,
+                                           start: Int, before: Int,
+                                           count: Int)
+                        = subscriber.onNext(s.toString())
             })
-        }
+        }).debounce(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ text ->
+
+                    val myCall = apiService.getByBase(mSpinner.text.toString())
+                    myCall.enqueue(object : Callback<ResponseAll> {
+                        override fun onResponse(call: Call<ResponseAll>?, response: Response<ResponseAll>?) {
+                            var value = 0.0
+                            if (eTxt.text.toString() != "")
+                                value = java.lang.Double.parseDouble(eTxt.text.toString())
+                            Log.e("${eTxt.text} ${mSpinner.text} ", "${(response!!.body()!!.rates!!.tRY!! * value)} Turkish Lira")
+                        }
+
+                        override fun onFailure(call: Call<ResponseAll>?, t: Throwable?) {
+
+                        }
+
+                    })
+
+
+                })
+
+
     }
 
     private fun init() {
