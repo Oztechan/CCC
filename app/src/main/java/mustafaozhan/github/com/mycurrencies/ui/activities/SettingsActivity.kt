8fd3afc646c25779bc53cb0b43139b68316910ea
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-02 21:02:41 +0300

    Adding select and deselect All option in Settings

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/SettingsActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/SettingsActivity.kt
@@ -4,31 +4,84 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.row_settings.view.*
 
 import mustafaozhan.github.com.mycurrencies.R
 import mustafaozhan.github.com.mycurrencies.model.data.Setting
 import mustafaozhan.github.com.mycurrencies.ui.adapters.SettingsAdapter
-import ninja.sakib.pultusorm.core.PultusORM
+import ninja.sakib.pultusorm.callbacks.Callback
+import ninja.sakib.pultusorm.core.*
+import ninja.sakib.pultusorm.exceptions.PultusORMException
+import org.jetbrains.anko.doAsync
 
 class SettingsActivity : AppCompatActivity() {
+    val settingsList = ArrayList<Setting>()
+    val mAdapter = SettingsAdapter(settingsList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val myDatabase = PultusORM("myDatabase.db", applicationContext.filesDir.absolutePath)
 
-        get()
+
+
+        getItems()
+
+        selectAll.setOnClickListener {
+            doAsync {
+                val updater: PultusORMUpdater = PultusORMUpdater.Builder()
+                        .set("isActive", "true")
+                        .build()
+
+                class ResponseCallback : Callback {
+                    override fun onSuccess(type: PultusORMQuery.Type) {
+                        log(type.name, "Success")
+                    }
+
+                    override fun onFailure(type: PultusORMQuery.Type, exception: PultusORMException) {
+                        log(type.name, "Failure")
+                        exception.printStackTrace()
+                    }
+                }
+                myDatabase.update(Setting(), updater, ResponseCallback())
+
+                runOnUiThread {
+                    getItems()
+                }
+            }
+        }
+        deSelectAll.setOnClickListener {
+            doAsync {
+                val updater: PultusORMUpdater = PultusORMUpdater.Builder()
+                        .set("isActive", "false")
+                        .build()
+
+                class ResponseCallback : Callback {
+                    override fun onSuccess(type: PultusORMQuery.Type) {
+                        log(type.name, "Success")
+                    }
+
+                    override fun onFailure(type: PultusORMQuery.Type, exception: PultusORMException) {
+                        log(type.name, "Failure")
+                        exception.printStackTrace()
+                    }
+                }
+                myDatabase.update(Setting(), updater, ResponseCallback())
+
+                runOnUiThread {
+                    getItems()
+                }
+            }
+        }
 
     }
 
-    private fun get() {
-
-        val settingsList = ArrayList<Setting>()
-        val mAdapter = SettingsAdapter(settingsList)
-
-
+    private fun getItems() {
         val myDatabase = PultusORM("myDatabase.db", applicationContext.filesDir.absolutePath)
+        settingsList.clear()
         val items = myDatabase.find(Setting())
-
         items.mapTo(settingsList) { it -> it as Setting }
         val mLayoutManager = LinearLayoutManager(applicationContext)
         mRecViewSettings.layoutManager = mLayoutManager
@@ -36,7 +89,6 @@ class SettingsActivity : AppCompatActivity() {
         mRecViewSettings.adapter = mAdapter
 
         mAdapter.notifyDataSetChanged()
-
     }
 
     override fun onBackPressed() {
