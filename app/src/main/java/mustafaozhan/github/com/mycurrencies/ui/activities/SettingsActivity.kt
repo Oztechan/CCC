bc1d8de490b6e1356f04286f15c2cc7c35c580e8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-03 13:58:15 +0300

    interstitial ads added

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/SettingsActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/activities/SettingsActivity.kt
@@ -11,6 +11,10 @@ import mustafaozhan.github.com.mycurrencies.model.data.Setting
 import mustafaozhan.github.com.mycurrencies.ui.adapters.SettingsAdapter
 import ninja.sakib.pultusorm.core.*
 import org.jetbrains.anko.doAsync
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+
 
 class SettingsActivity : AppCompatActivity() {
     private val settingsList = ArrayList<Setting>()
@@ -63,7 +67,18 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
+        val interstitial = InterstitialAd(this@SettingsActivity)
+        interstitial.adUnitId =resources.getString(R.string.interstitial)
+        val adRequest1 = AdRequest.Builder().build()
+        interstitial.loadAd(adRequest1)
+        interstitial.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                if (interstitial.isLoaded) {
+                    interstitial.show()
+                }
+            }
+        }
+
         finish()
     }
 }
