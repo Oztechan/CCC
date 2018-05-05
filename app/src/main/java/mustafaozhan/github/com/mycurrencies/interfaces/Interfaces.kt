baf35571e9aaa96d3b68e4ceb33f728dd08d1638
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-05-05 20:56:25 +0200

    Reset project for MVVM Architecture

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/interfaces/Interfaces.kt
+++
@@ -1,8 +0,0 @@
-package mustafaozhan.github.com.mycurrencies.interfaces
-
-/**
- * Created by Mustafa Ozhan on 11/26/17 at 12:16 AM on Arch Linux.
- */
-interface SettingsAdapterCallback {
-    fun onSettingsUpdated()
-}
