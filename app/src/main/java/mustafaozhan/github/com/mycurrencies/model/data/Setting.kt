7b80577da009cd7ec38bbeed1971ef3e0e333e66
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-07-10 21:31:06 +0200

    MVVM Initial Commit

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/model/data/Setting.kt
+++
@@ -1,8 +0,0 @@
-package mustafaozhan.github.com.mycurrencies.model.data
-
-import ninja.sakib.pultusorm.annotations.PrimaryKey
-
-/**
- * Created by Mustafa Ozhan on 10/9/17 at 1:01 PM on Arch Linux.
- */
-data class Setting(@PrimaryKey var name: String? = null, var isActive: String = "true")
