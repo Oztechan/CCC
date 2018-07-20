ce9d8fe4dc287507eb1b3162aeef53cc820f548c
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-07-20 11:04:10 +0200

    Splash Screen Added

---
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package mustafaozhan.github.com.mycurrencies.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import mustafaozhan.github.com.mycurrencies.main.activity.MainActivity
+
+/**
+ * Created by Mustafa Ozhan on 2018-07-20.
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
