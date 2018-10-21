06b63cda40b911e05bd62959c51631c42471a37c
Mustafa Ozhan <mustafa.ozhan@adastra.one>
2018-10-21 11:52:43 +0200

    new backend integration

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/base/api/exchangerates/ExchangeRatesApiServices.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/base/api/exchangerates/ExchangeRatesApiServices.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
  * Created by Mustafa Ozhan on 2018-07-12.
  */
 interface ExchangeRatesApiServices {
-    @GET("latest")
+    @GET("byBase")
     fun getAllOnBase(@Query("base") base: Currencies):
             Observable<CurrencyResponse>
 }
