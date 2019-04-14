5668046e88995f3c0e3c9ccd698699a12fd08239
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-04-14 21:49:40 +0200

    Max input and moving symbols to end of row

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/extensions/ViewExtensions.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/extensions/ViewExtensions.kt
@@ -11,11 +11,15 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import mustafaozhan.github.com.mycurrencies.R
+import mustafaozhan.github.com.mycurrencies.main.activity.MainActivity
 import java.io.FileNotFoundException
 
 /**
  * Created by Mustafa Ozhan on 2018-07-20.
  */
+
+const val MAX_DIGIT = 18
+
 fun ImageView.setBackgroundByName(name: String) =
     setImageResource(context.getImageResourceByName(name))
 
@@ -31,16 +35,12 @@ fun Context.getImageResourceByName(name: String): Int =
         R.drawable.transparent
     }
 
-fun Context.getStringByName(name: String): String = getString(
-    resources.getIdentifier(
-        name, "string", packageName
-    )
-)
-
 @SuppressLint("SetTextI18n")
-fun TextView.addText(str: String) {
-    text = text.toString() + str
-}
+fun TextView.addText(str: String) =
+    if (text.toString().length < MAX_DIGIT)
+        text = text.toString() + str
+    else
+        (context as MainActivity).snacky(context.getString(R.string.max_input), isLong = false)
 
 fun AdView.loadAd(adId: Int) {
     MobileAds.initialize(context, resources.getString(adId))
