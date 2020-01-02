924a18f88d6579000ac066ff8e2e10dfa6822969
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-01-02 22:19:12 +0100

    adding animation for settings recycler view back

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/main/fragment/settings/SettingsAdapter.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/ui/main/fragment/settings/SettingsAdapter.kt
@@ -2,6 +2,7 @@ package mustafaozhan.github.com.mycurrencies.ui.main.fragment.settings
 
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.item_setting.view.checkBox
 import kotlinx.android.synthetic.main.item_setting.view.img_icon
 import kotlinx.android.synthetic.main.item_setting.view.txt_setting_item
@@ -18,6 +19,16 @@ class SettingsAdapter : BaseRecyclerViewAdapter<Currency>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<Currency> =
         RatesViewHolder(getViewHolderView(parent, R.layout.item_setting))
+
+    override fun onBindViewHolder(holder: BaseViewHolder<Currency>, position: Int) {
+        holder.itemView.startAnimation(
+            AnimationUtils.loadAnimation(
+                holder.itemView.context,
+                R.anim.fall_down
+            )
+        )
+        super.onBindViewHolder(holder, position)
+    }
 
     class RatesViewHolder(itemView: View) : BaseViewHolder<Currency>(itemView) {
         override fun bind(item: Currency) {
