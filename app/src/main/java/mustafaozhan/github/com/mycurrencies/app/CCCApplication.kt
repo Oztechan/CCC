dda845921b9f2aec1d7b10fa336d7f2f5a786559
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-02-22 15:52:55 +0100

    build fix

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/app/CCCApplication.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/app/CCCApplication.kt
@@ -1,14 +1,11 @@
 package mustafaozhan.github.com.mycurrencies.app
 
-import android.app.Activity
-import androidx.fragment.app.Fragment
 import androidx.multidex.MultiDexApplication
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import io.fabric.sdk.android.Fabric
 import mustafaozhan.github.com.mycurrencies.BuildConfig
 import mustafaozhan.github.com.mycurrencies.di.DaggerAppComponent
@@ -21,16 +18,13 @@ import javax.inject.Inject
 /**
  * Created by Mustafa Ozhan on 7/10/18 at 9:43 PM on Arch Linux wit Love <3.
  */
-class CCCApplication : MultiDexApplication(), HasActivityInjector, HasSupportFragmentInjector {
+class CCCApplication : MultiDexApplication(), HasAndroidInjector {
     companion object {
         lateinit var instance: CCCApplication
     }
 
     @Inject
-    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
-
-    @Inject
-    lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
@@ -54,9 +48,5 @@ class CCCApplication : MultiDexApplication(), HasActivityInjector, HasSupportFra
         Thread.setDefaultUncaughtExceptionHandler(WatchDogHandler())
     }
 
-    override fun activityInjector(): AndroidInjector<Activity> =
-        activityDispatchingAndroidInjector
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
-        fragmentDispatchingAndroidInjector
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
