8f01a5f9afb30c56ee35e71a0570297d2498e10b
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-03-09 15:52:31 +0100

    data binding base classes edited

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/base/activity/BaseDataBindingActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/base/activity/BaseDataBindingActivity.kt
@@ -1,24 +1,19 @@
 package mustafaozhan.github.com.mycurrencies.base.activity
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 abstract class BaseDataBindingActivity<TDataBinding : ViewDataBinding> : BaseActivity() {
 
+    protected lateinit var binding: TDataBinding
+    abstract fun bind(): TDataBinding
+    abstract fun onBinding(dataBinding: ViewDataBinding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind()
+        binding = bind()
+        onBinding(binding)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
     }
-
-    private fun bind() =
-        getLayoutResId()?.let {
-            DataBindingUtil
-                .setContentView<TDataBinding>(
-                    this,
-                    it
-                ).apply {
-                    //    todo                setVariable(BR.viewModel, viewModel)
-                }
-        }
 }
--- app/src/main/java/mustafaozhan/github/com/mycurrencies/base/fragment/BaseDataBindingFragment.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/base/fragment/BaseDataBindingFragment.kt
@@ -2,22 +2,25 @@ package mustafaozhan.github.com.mycurrencies.base.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 abstract class BaseDataBindingFragment<TDataBinding : ViewDataBinding> : BaseFragment() {
+
+    protected lateinit var binding: TDataBinding
+
+    abstract fun bind(container: ViewGroup?): TDataBinding
+    abstract fun onBinding(dataBinding: TDataBinding)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = DataBindingUtil.inflate<TDataBinding>(
-        inflater,
-        getLayoutResId(),
-        container,
-        false
-    ).apply {
-        //   todo     setVariable(BR.viewModel, viewModel)
-    }.root
+    ): View {
+        binding = bind(container)
+        binding.lifecycleOwner = viewLifecycleOwner
+        onBinding(binding)
+        return binding.root
+    }
 }
