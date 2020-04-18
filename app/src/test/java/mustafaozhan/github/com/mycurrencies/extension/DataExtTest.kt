6740403d8aac0ecb3579a8c29eb9ed7c65fd3224
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-04-18 22:55:22 +0200

    catching up to master

--- app/src/test/java/mustafaozhan/github/com/mycurrencies/extension/DataExtTest.kt
+++ app/src/test/java/mustafaozhan/github/com/mycurrencies/extension/DataExtTest.kt
@@ -1,4 +1,4 @@
-package mustafaozhan.github.com.mycurrencies.function.extension
+package mustafaozhan.github.com.mycurrencies.extension
 
 import mustafaozhan.github.com.mycurrencies.model.Rates
 import org.junit.Assert.assertEquals
