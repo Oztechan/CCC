3749081d8f19bbc37abc66e8ab66b3d7c4e3f13f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-05-30 14:31:12 +0200

    preferences cleaned from old logic

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/data/preferences/PreferencesRepository.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/data/preferences/PreferencesRepository.kt
@@ -6,6 +6,8 @@ package mustafaozhan.github.com.mycurrencies.data.preferences
 import android.content.Context
 import com.github.mustafaozhan.basemob.data.preferences.BasePreferencesRepository
 import mustafaozhan.github.com.mycurrencies.model.Currencies
+import mustafaozhan.github.com.mycurrencies.util.getOldBaseCurrency
+import mustafaozhan.github.com.mycurrencies.util.getOldFirstRun
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,43 +17,27 @@ class PreferencesRepository
     context: Context
 ) : BasePreferencesRepository(context) {
     companion object {
-        const val GENERAL_SHARED_PREFS = "GENERAL_SHARED_PREFS"
-        private const val MAIN_DATA = "MAIN_DATA"
-        private const val FIRST_RUN = "firs_run"
-        private const val CURRENT_BASE = "current_base"
-        private const val AD_FREE_DATE = "ad_free_date"
+        private const val KEY_APPLICATION_PREFERENCES = "application_preferences"
+        private const val KEY_FIRST_RUN = "firs_run"
+        private const val KEY_CURRENT_BASE = "current_base"
+        private const val KEY_AD_FREE_DATE = "ad_free_date"
         private const val DAY = 24 * 60 * 60 * 1000.toLong()
     }
 
     override val preferencesName: String
-        get() = GENERAL_SHARED_PREFS
+        get() = KEY_APPLICATION_PREFERENCES
 
     var firstRun
-        get() = getValue(FIRST_RUN, getMainDataMap()?.get(0) == true.toString())
-        set(value) = setValue(FIRST_RUN, value)
+        get() = getValue(KEY_FIRST_RUN, getOldFirstRun(context) != true.toString())
+        set(value) = setValue(KEY_FIRST_RUN, value)
 
     var currentBase
-        get() = getValue(CURRENT_BASE, getMainDataMap()?.get(1) ?: Currencies.EUR.toString())
-        set(value) = setValue(CURRENT_BASE, value)
+        get() = getValue(KEY_CURRENT_BASE, getOldBaseCurrency(context) ?: Currencies.EUR.toString())
+        set(value) = setValue(KEY_CURRENT_BASE, value)
 
     var adFreeActivatedDate
-        get() = getValue(AD_FREE_DATE, 0.toLong())
-        set(value) = setValue(AD_FREE_DATE, value)
+        get() = getValue(KEY_AD_FREE_DATE, 0.toLong())
+        set(value) = setValue(KEY_AD_FREE_DATE, value)
 
     fun isRewardExpired() = System.currentTimeMillis() - adFreeActivatedDate > DAY
-
-    private fun getMainDataMap(): MutableList<String>? {
-        val mainDataList: MutableList<String>? = null
-        getValue(MAIN_DATA, "")
-            .dropLast(1).drop(1)
-            .replace("\"", "")
-            .split(",")
-            .take(2)
-            .forEach { value ->
-                value.split(":").let {
-                    mainDataList?.add(it[1])
-                }
-            }
-        return mainDataList
-    }
 }
