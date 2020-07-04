53b679ddfc3bc4ec9d7ff9e5ec0509e08820d185
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-07-05 00:33:17 +0200

    added missed event tests

--- ui/src/test/java/com/github/mustafaozhan/ui/viewmodel/SettingsViewModelTest.kt
+++ ui/src/test/java/com/github/mustafaozhan/ui/viewmodel/SettingsViewModelTest.kt
@@ -4,12 +4,16 @@
 package com.github.mustafaozhan.ui.viewmodel
 
 import com.github.mustafaozhan.data.db.CurrencyDao
+import com.github.mustafaozhan.data.model.Currency
 import com.github.mustafaozhan.data.preferences.PreferencesRepository
+import com.github.mustafaozhan.ui.main.settings.FewCurrencyEffect
 import com.github.mustafaozhan.ui.main.settings.SettingsViewModel
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.ObsoleteCoroutinesApi
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -29,5 +33,34 @@ class SettingsViewModelTest : BaseViewModelTest<SettingsViewModel>() {
     fun setup() {
         MockKAnnotations.init(this)
         viewModel = SettingsViewModel(preferencesRepository, currencyDao)
+    }
+
+    // Event
+    @Test
+    fun `on long click`() = with(viewModel) {
+        val currentValue = viewModel.state.selectionVisibility.value
+        getEvent().onItemLongClick()
+        Assert.assertEquals(
+            !(currentValue ?: return@with),
+            viewModel.state.selectionVisibility.value
+        )
+    }
+
+    @Test
+    fun `update all currencies state`() {
+        Assert.assertEquals(Unit, viewModel.getEvent().updateAllCurrenciesState(true))
+        Assert.assertEquals(Unit, viewModel.getEvent().updateAllCurrenciesState(false))
+    }
+
+    @Test
+    fun `on item click`() {
+        val currency = Currency("EUR", "Euro", "€")
+        Assert.assertEquals(Unit, viewModel.getEvent().onItemClick(currency))
+    }
+
+    @Test
+    fun `on done click`() {
+        viewModel.getEvent().onDoneClick()
+        Assert.assertEquals(FewCurrencyEffect, viewModel.effect.value)
     }
 }
