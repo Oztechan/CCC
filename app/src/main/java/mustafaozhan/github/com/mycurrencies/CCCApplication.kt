27dbe9e794fdfd87e2991df53b3ef4ef2f230521
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-07-10 21:06:03 +0200

    removing strict mode

--- app/src/main/java/mustafaozhan/github/com/mycurrencies/CCCApplication.kt
+++ app/src/main/java/mustafaozhan/github/com/mycurrencies/CCCApplication.kt
@@ -3,7 +3,6 @@
  */
 package mustafaozhan.github.com.mycurrencies
 
-import android.os.StrictMode
 import androidx.multidex.MultiDexApplication
 import com.github.mustafaozhan.logmob.initLogMob
 import dagger.android.AndroidInjector
@@ -30,21 +29,6 @@ class CCCApplication : MultiDexApplication(), HasAndroidInjector {
             enableCrashlytics = true,
             enableAnalytics = true
         )
-
-        if (BuildConfig.DEBUG) {
-            StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .build()
-            )
-            StrictMode.setVmPolicy(
-                StrictMode.VmPolicy.Builder()
-                    .detectAll()
-                    .penaltyLog()
-                    .build()
-            )
-        }
     }
 
     override fun androidInjector(): AndroidInjector<Any> = androidInjector
