7933fbf5c13ac2ee382223753c84697b3dd58c04
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2020-12-04 23:24:13 +0300

    test fix

--- android/src/test/java/com/github/mustafaozhan/ccc/android/viewmodel/CalculatorViewModelTest.kt
+++ android/src/test/java/com/github/mustafaozhan/ccc/android/viewmodel/CalculatorViewModelTest.kt
@@ -10,7 +10,7 @@ import com.github.mustafaozhan.ccc.android.ui.calculator.OpenBarEffect
 import com.github.mustafaozhan.ccc.android.ui.calculator.OpenSettingsEffect
 import com.github.mustafaozhan.ccc.android.ui.calculator.ShowRateEffect
 import com.github.mustafaozhan.ccc.client.repo.SettingsRepository
-import com.github.mustafaozhan.data.api.ApiRepository
+import com.github.mustafaozhan.ccc.common.api.ApiRepository
 import com.github.mustafaozhan.data.db.CurrencyDao
 import com.github.mustafaozhan.data.db.OfflineRatesDao
 import com.github.mustafaozhan.data.model.Currency
--- android/src/test/java/com/github/mustafaozhan/ccc/android/viewmodel/SettingsViewModelTest.kt
+++ android/src/test/java/com/github/mustafaozhan/ccc/android/viewmodel/SettingsViewModelTest.kt
@@ -16,7 +16,7 @@ import com.github.mustafaozhan.ccc.android.ui.settings.SupportUsEffect
 import com.github.mustafaozhan.ccc.android.ui.settings.ThemeDialogEffect
 import com.github.mustafaozhan.ccc.android.util.DAY
 import com.github.mustafaozhan.ccc.client.repo.SettingsRepository
-import com.github.mustafaozhan.data.api.ApiRepository
+import com.github.mustafaozhan.ccc.common.api.ApiRepository
 import com.github.mustafaozhan.data.db.CurrencyDao
 import com.github.mustafaozhan.data.db.OfflineRatesDao
 import com.github.mustafaozhan.data.util.dateStringToFormattedString
