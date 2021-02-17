ea6138c104b3fb0366d6682a7b67a08cd4a03b3f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-02-17 16:52:09 +0100

    creating viewModelDefinition

--- client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/di/Koin.kt
+++ client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/di/Koin.kt
@@ -4,20 +4,42 @@
 
 package com.github.mustafaozhan.ccc.client.di
 
+import com.github.mustafaozhan.ccc.client.base.BaseViewModel
+import com.github.mustafaozhan.ccc.client.viewmodel.BarViewModel
+import com.github.mustafaozhan.ccc.client.viewmodel.CalculatorViewModel
+import com.github.mustafaozhan.ccc.client.viewmodel.CurrenciesViewModel
+import com.github.mustafaozhan.ccc.client.viewmodel.MainViewModel
+import com.github.mustafaozhan.ccc.client.viewmodel.SettingsViewModel
 import com.github.mustafaozhan.ccc.common.di.getDependency
 import com.github.mustafaozhan.ccc.common.di.initCommon
 import com.github.mustafaozhan.ccc.common.log.kermit
 import kotlin.reflect.KClass
 import org.koin.core.Koin
 import org.koin.core.KoinApplication
+import org.koin.core.definition.BeanDefinition
+import org.koin.core.definition.Definition
 import org.koin.core.module.Module
+import org.koin.core.qualifier.Qualifier
+import org.koin.dsl.module
 
 fun initClient(appModule: Module, useFakes: Boolean = false): KoinApplication = initCommon(
-    appModule.plus(clientModule), useFakes
+    appModule.plus(viewModelModule), useFakes
 ).also {
     kermit.d { "Koin initClient" }
 }
 
-expect val clientModule: Module
+private val viewModelModule = module {
+    viewModelDefinition { SettingsViewModel(get(), get(), get(), get()) }
+    viewModelDefinition { MainViewModel(get()) }
+    viewModelDefinition { CurrenciesViewModel(get(), get()) }
+    viewModelDefinition { CalculatorViewModel(get(), get(), get(), get()) }
+    viewModelDefinition { BarViewModel(get()) }
+}
+
+expect inline fun <reified T : BaseViewModel> Module.viewModelDefinition(
+    qualifier: Qualifier? = null,
+    override: Boolean = false,
+    noinline definition: Definition<T>
+): BeanDefinition<T>
 
 fun <T> Koin.getDependency(clazz: KClass<*>) = getDependency<T>(clazz)
--- client/src/jsMain/kotlin/com/github/mustafaozhan/ccc/client/di/KoinJS.kt
+++ client/src/jsMain/kotlin/com/github/mustafaozhan/ccc/client/di/KoinJS.kt
@@ -4,13 +4,12 @@
 
 package com.github.mustafaozhan.ccc.client.di
 
-import com.github.mustafaozhan.ccc.client.viewmodel.BarViewModel
-import com.github.mustafaozhan.ccc.client.viewmodel.CalculatorViewModel
-import com.github.mustafaozhan.ccc.client.viewmodel.CurrenciesViewModel
-import com.github.mustafaozhan.ccc.client.viewmodel.MainViewModel
-import com.github.mustafaozhan.ccc.client.viewmodel.SettingsViewModel
+import com.github.mustafaozhan.ccc.client.base.BaseViewModel
 import com.github.mustafaozhan.ccc.common.log.kermit
+import org.koin.core.definition.BeanDefinition
+import org.koin.core.definition.Definition
 import org.koin.core.module.Module
+import org.koin.core.qualifier.Qualifier
 import org.koin.dsl.module
 import org.w3c.dom.Storage
 
@@ -20,10 +19,8 @@ fun initJS(storage: Storage) = initClient(
     kermit.d { "KoinJS initJS" }
 }
 
-actual val clientModule: Module = module {
-    single { SettingsViewModel(get(), get(), get(), get()) }
-    single { MainViewModel(get()) }
-    single { CurrenciesViewModel(get(), get()) }
-    single { CalculatorViewModel(get(), get(), get(), get()) }
-    single { BarViewModel(get()) }
-}
+actual inline fun <reified T : BaseViewModel> Module.viewModelDefinition(
+    qualifier: Qualifier?,
+    override: Boolean,
+    noinline definition: Definition<T>
+): BeanDefinition<T> = single(qualifier = qualifier, override = override, definition = definition)
