ebcd3c2548e342a82330ca9ac152a942d220ffbf
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-06-03 22:26:03 +0300

    changing the way of getting commit count

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -2,9 +2,7 @@
  * Copyright (c) 2021 Mustafa Ozhan. All rights reserved.
  */
 import org.gradle.api.Project
-import java.io.File
-import java.io.IOException
-import java.util.concurrent.TimeUnit
+import java.io.ByteArrayOutputStream
 
 object ProjectSettings {
 
@@ -21,27 +19,12 @@ object ProjectSettings {
 
     fun getVersionName(project: Project) = "$mayorVersion.$minorVersion.${gitCommitCount(project)}"
 
-    private fun gitCommitCount(project: Project) =
-        "git rev-list --first-parent --count HEAD"
-            .runCommand(project.rootDir).trim().let {
-                if (it.isEmpty()) 1.toString() else it
-            }
-
-    private fun String.runCommand(
-        workingDir: File = File("."),
-        timeoutAmount: Long = 60,
-        timeoutUnit: TimeUnit = TimeUnit.SECONDS
-    ): String = ProcessBuilder(split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
-        .directory(workingDir)
-        .redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .redirectError(ProcessBuilder.Redirect.PIPE)
-        .start()
-        .apply { waitFor(timeoutAmount, timeoutUnit) }
-        .run {
-            val error = errorStream.bufferedReader().readText().trim()
-            if (error.isNotEmpty()) {
-                throw IOException(error)
-            }
-            inputStream.bufferedReader().readText().trim()
+    private fun gitCommitCount(project: Project): String {
+        val os = ByteArrayOutputStream()
+        project.exec {
+            commandLine = "git rev-list --first-parent --count HEAD".split(" ")
+            standardOutput = os
         }
+        return String(os.toByteArray()).trim()
+    }
 }
