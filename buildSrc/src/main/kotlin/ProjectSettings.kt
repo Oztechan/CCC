35427923492d7327bf715ab89f4f0dc2d92391fc
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-09-18 20:46:20 +0200

    increasing compile&target sdk versions and library updates

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2021 Mustafa Ozhan. All rights reserved.
  */
-import org.gradle.api.Project
 import java.io.ByteArrayOutputStream
+import org.gradle.api.Project
 
 object ProjectSettings {
 
@@ -14,9 +14,9 @@ object ProjectSettings {
 
     const val PROJECT_ID = "mustafaozhan.github.com.mycurrencies"
     const val PACKAGE_NAME = "com.github.mustafaozhan.ccc"
-    const val COMPILE_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 31
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 30
+    const val TARGET_SDK_VERSION = 31
 
     fun getVersionCode(project: Project) = gitCommitCount(project).toInt()
 
