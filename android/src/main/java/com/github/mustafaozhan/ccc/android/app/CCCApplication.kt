a77401351c34a034b3638393d3398cf77e5496d1
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-10-04 21:34:00 +0200

    Adaptation to new AdManager

--- android/src/main/java/com/github/mustafaozhan/ccc/android/app/CCCApplication.kt
+++ android/src/main/java/com/github/mustafaozhan/ccc/android/app/CCCApplication.kt
@@ -4,13 +4,11 @@
 package com.github.mustafaozhan.ccc.android.app
 
 import android.app.Application
-import com.github.mustafaozhan.ad.initMobileAds
-import com.github.mustafaozhan.billing.BillingManager
+import com.github.mustafaozhan.ccc.android.di.getPlatformModule
 import com.github.mustafaozhan.ccc.client.di.initAndroid
 import com.github.mustafaozhan.logmob.initCrashlytics
 import com.github.mustafaozhan.logmob.initLogger
 import com.github.mustafaozhan.logmob.kermit
-import org.koin.dsl.module
 
 @Suppress("unused")
 class CCCApplication : Application() {
@@ -19,12 +17,9 @@ class CCCApplication : Application() {
         super.onCreate()
         initLogger()
         initAndroid(
-            context = this@CCCApplication,
-            platformModule = module {
-                single { BillingManager(get()) }
-            }
+            context = this,
+            platformModule = getPlatformModule(this)
         )
-        initMobileAds(this)
         initCrashlytics(this, enableAnalytics = true)
         kermit.d { "CCCApplication onCreate" }
     }
