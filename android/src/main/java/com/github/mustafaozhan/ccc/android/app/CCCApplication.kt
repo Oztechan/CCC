749384b9b45c303243a75b170f5c89e175ec9989
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-11-25 18:50:31 +0100

    [#222] add StrictMode

--- android/src/main/java/com/github/mustafaozhan/ccc/android/app/CCCApplication.kt
+++ android/src/main/java/com/github/mustafaozhan/ccc/android/app/CCCApplication.kt
@@ -5,18 +5,26 @@ package com.github.mustafaozhan.ccc.android.app
 
 import android.app.Application
 import android.os.Build
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import co.touchlab.kermit.Logger
 import com.github.mustafaozhan.ad.initAds
 import com.github.mustafaozhan.ccc.android.di.platformModule
 import com.github.mustafaozhan.ccc.client.di.initAndroid
 import com.github.mustafaozhan.logmob.initCrashlytics
 import com.github.mustafaozhan.logmob.initLogger
+import mustafaozhan.github.com.mycurrencies.BuildConfig
 
 @Suppress("unused")
 class CCCApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            enableStrictMode()
+        }
 
         initLogger().let {
             it.i { "Logger initialized" }
@@ -35,6 +43,21 @@ class CCCApplication : Application() {
         initAndroid(
             context = this,
             platformModule = platformModule
+        )
+    }
+
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
+        )
+        StrictMode.setVmPolicy(
+            VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .build()
         )
     }
 }
