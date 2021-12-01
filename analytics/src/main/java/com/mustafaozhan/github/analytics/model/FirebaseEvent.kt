89e2333d8b7a4dfc67fb3e4d26ab52bac1b470a8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-01 22:12:26 +0100

    [#260] track offline sync event

--- analytics/src/main/java/com/mustafaozhan/github/analytics/model/FirebaseEvent.kt
+++ analytics/src/main/java/com/mustafaozhan/github/analytics/model/FirebaseEvent.kt
@@ -2,5 +2,6 @@ package com.mustafaozhan.github.analytics.model
 
 enum class FirebaseEvent(val key: String) {
     BASE_CHANGE("base_change"),
-    SHOW_CONVERSION("show_conversion")
+    SHOW_CONVERSION("show_conversion"),
+    OFFLINE_SYNC("offline_sync")
 }
