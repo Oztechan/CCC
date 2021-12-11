933d5447ba954b3ea56eead327a8811175b9df2b
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-12 00:02:30 +0100

    [#292] apply new detekt rules (#293)

--- common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/base/BaseRepositoryTest.kt
+++ common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/base/BaseRepositoryTest.kt
@@ -9,11 +9,11 @@ import com.github.mustafaozhan.ccc.common.di.modules.apiModule
 import com.github.mustafaozhan.ccc.common.di.modules.getDatabaseModule
 import com.github.mustafaozhan.ccc.common.di.modules.getSettingsModule
 import com.github.mustafaozhan.logmob.initLogger
-import kotlin.test.AfterTest
-import kotlin.test.BeforeTest
 import org.koin.core.Koin
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 
 abstract class BaseRepositoryTest<SubjectType> {
     protected lateinit var koin: Koin
