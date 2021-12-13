05fb97f4e709fdb3aa2dfb1cfc7b5aefe14f3a25
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-14 00:03:07 +0100

    [#303] hms build issue fix (#312)

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,11 +40,10 @@ allprojects {
     repositories {
         mavenCentral()
         google()
+        maven("https://developer.huawei.com/repo/")
         maven("https://dl.bintray.com/ekito/koin")
         maven("https://dl.bintray.com/icerockdev/moko")
         maven("https://kotlin.bintray.com/kotlinx/")
-        // todo https://github.com/Oztechan/CCC/issues/303
-        // maven("https://developer.huawei.com/repo/")
     }
 }
 
