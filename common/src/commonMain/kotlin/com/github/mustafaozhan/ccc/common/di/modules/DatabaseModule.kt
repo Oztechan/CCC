637788dfe7967c62404f0b098c33bb3367437ab0
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-18 00:02:16 +0200

    [#334] remove fakes and fakes related logic
    (cherry picked from commit 2be56d2c492d20a9016aa0d1a1480a6ea86b3826)

--- common/src/commonMain/kotlin/com/github/mustafaozhan/ccc/common/di/modules/DatabaseModule.kt
+++ common/src/commonMain/kotlin/com/github/mustafaozhan/ccc/common/di/modules/DatabaseModule.kt
@@ -5,24 +5,19 @@ import com.github.mustafaozhan.ccc.common.db.currency.CurrencyRepositoryImpl
 import com.github.mustafaozhan.ccc.common.db.offlinerates.OfflineRatesRepository
 import com.github.mustafaozhan.ccc.common.db.offlinerates.OfflineRatesRepositoryImpl
 import com.github.mustafaozhan.ccc.common.db.sql.CurrencyConverterCalculatorDatabase
-import com.github.mustafaozhan.ccc.common.di.getDatabaseDefinition
-import com.github.mustafaozhan.ccc.common.fake.FakeCurrencyQueries
-import com.github.mustafaozhan.ccc.common.fake.FakeOfflineRatesQueries
+import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
 private const val DATABASE_NAME = "application_database.sqlite"
 
-fun getDatabaseModule(forTest: Boolean = false) = module {
-    if (forTest) {
-        single { FakeCurrencyQueries.getCurrencyQueries() }
-        single { FakeOfflineRatesQueries.getOfflineRatesQueries() }
-    } else {
-        single { get<CurrencyConverterCalculatorDatabase>().currencyQueries }
-        single { get<CurrencyConverterCalculatorDatabase>().offlineRatesQueries }
-    }
+fun getDatabaseModule() = module {
+    single { get<CurrencyConverterCalculatorDatabase>().currencyQueries }
+    single { get<CurrencyConverterCalculatorDatabase>().offlineRatesQueries }
 
     single<CurrencyRepository> { CurrencyRepositoryImpl(get()) }
     single<OfflineRatesRepository> { OfflineRatesRepositoryImpl(get()) }
 
-    getDatabaseDefinition(DATABASE_NAME)
+    single { provideDatabase(DATABASE_NAME) }
 }
+
+expect fun Scope.provideDatabase(databaseName: String): CurrencyConverterCalculatorDatabase
