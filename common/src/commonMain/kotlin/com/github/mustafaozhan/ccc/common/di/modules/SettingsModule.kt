ba974ed0cc97040010744591478d836e0fe2f0f8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-19 13:24:15 +0200

    [#334] remove fakes and fakes related logic (#336)

--- common/src/commonMain/kotlin/com/github/mustafaozhan/ccc/common/di/modules/SettingsModule.kt
+++ common/src/commonMain/kotlin/com/github/mustafaozhan/ccc/common/di/modules/SettingsModule.kt
@@ -1,17 +1,14 @@
 package com.github.mustafaozhan.ccc.common.di.modules
 
-import com.github.mustafaozhan.ccc.common.di.getSettingsDefinition
-import com.github.mustafaozhan.ccc.common.fake.FakeSettings
 import com.github.mustafaozhan.ccc.common.settings.SettingsRepository
 import com.github.mustafaozhan.ccc.common.settings.SettingsRepositoryImp
+import com.russhwolf.settings.Settings
+import org.koin.core.scope.Scope
 import org.koin.dsl.module
 
-fun getSettingsModule(forTest: Boolean = false) = module {
-    if (forTest) {
-        single { FakeSettings.getSettings() }
-    } else {
-        getSettingsDefinition()
-    }
-
+fun getSettingsModule() = module {
+    single { provideSettings() }
     single<SettingsRepository> { SettingsRepositoryImp(get()) }
 }
+
+expect fun Scope.provideSettings(): Settings
