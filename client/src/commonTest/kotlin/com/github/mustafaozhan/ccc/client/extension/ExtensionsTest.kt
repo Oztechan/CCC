26603632ef2cf56133902c79dac251574cec6863
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2021-12-21 13:45:13 +0200

    [#347] increase coverage in Client Extensions

--- client/src/commonTest/kotlin/com/github/mustafaozhan/ccc/client/extension/ExtensionsTest.kt
+++ client/src/commonTest/kotlin/com/github/mustafaozhan/ccc/client/extension/ExtensionsTest.kt
@@ -12,7 +12,6 @@ import com.github.mustafaozhan.ccc.client.util.doubleDigits
 import com.github.mustafaozhan.ccc.client.util.getConversionByName
 import com.github.mustafaozhan.ccc.client.util.getCurrencyConversionByRate
 import com.github.mustafaozhan.ccc.client.util.getFormatted
-import com.github.mustafaozhan.ccc.client.util.isEmptyOrNullString
 import com.github.mustafaozhan.ccc.client.util.isRewardExpired
 import com.github.mustafaozhan.ccc.client.util.isWeekPassed
 import com.github.mustafaozhan.ccc.client.util.toDateString
@@ -50,6 +49,17 @@ class ExtensionsTest {
         assertEquals(0.0, rates.calculateResult(target, ""))
         assertEquals(0.0, rates.calculateResult(target, null))
         assertEquals(25.0, rates.calculateResult(target, "5.0"))
+        assertEquals(0.0, rates.calculateResult("", "5.0"))
+        assertEquals(0.0, null.calculateResult(target, "5.0"))
+
+        val inputOne = "1.0"
+        assertEquals(rates.getConversionByName(target), rates.calculateResult(target, inputOne))
+
+        val inputTwo = "2.0"
+        assertEquals(
+            rates.getConversionByName(target)?.times(2),
+            rates.calculateResult(target, inputTwo)
+        )
     }
 
     @Test
@@ -59,7 +69,7 @@ class ExtensionsTest {
         val rates = Rates(base, null, usd = 5.0)
 
         assertEquals(
-            "1 EUR = 5.0 USD Dollar \$",
+            "1 $base = ${rates.getConversionByName(currency.name)} ${currency.getVariablesOneLine()}",
             currency.getCurrencyConversionByRate(base, rates)
         )
     }
@@ -73,9 +83,12 @@ class ExtensionsTest {
             add(Currency(CurrencyType.EUR.toString(), "", "", 1.2, true))
             add(Currency(CurrencyType.USD.toString(), "", "", 1.2, false))
             add(Currency(CurrencyType.TRY.toString(), "", "", Double.NaN, true))
-            add(Currency(CurrencyType.CZK.toString(), "", "", 0.0, true))
+            add(Currency(CurrencyType.GGP.toString(), "", "", 0.0, true))
+            add(Currency(CurrencyType.RON.toString(), "", "", "0.0".toDouble(), true))
+            add(Currency(CurrencyType.CZK.toString(), "", "", 0.toDouble(), true))
         }
         assertEquals(mutableListOf(), list.toValidList(base))
+        assertEquals(mutableListOf(), null.toValidList(base))
     }
 
     @Test
@@ -116,19 +129,29 @@ class ExtensionsTest {
     }
 
     @Test
-    fun isEmptyOrNullString() {
-        assertEquals(true, "".isEmptyOrNullString())
-        assertEquals(true, "null".isEmptyOrNullString())
-        assertEquals(true, "Null".isEmptyOrNullString())
-        assertEquals(true, "NULL".isEmptyOrNullString())
-    }
-
-    @Test
     fun getConversionByName() {
-        val rates = Rates("EUR", "", usd = 5.0, eur = 12.2)
+        val rates = Rates(
+            "base", "12.12.21", 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,
+            13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0,
+            27.0, 28.0, 29.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0,
+            41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0,
+            55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 68.0,
+            69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0, 81.0, 82.0,
+            83.0, 84.0, 85.0, 86.0, 87.0, 88.0, 89.0, 90.0, 91.0, 92.0, 93.0, 94.0, 95.0, 96.0,
+            97.0, 98.0, 99.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0,
+            109.0, 110.0, 111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0,
+            121.0, 122.0, 123.0, 124.0, 125.0, 126.0, 127.0, 128.0, 129.0, 130.0, 131.0, 132.0,
+            133.0, 134.0, 135.0, 136.0, 137.0, 138.0, 139.0, 140.0, 141.0, 142.0, 143.0, 144.0,
+            145.0, 146.0, 147.0, 148.0, 149.0, 150.0, 151.0, 152.0, 153.0, 154.0, 155.0, 156.0,
+            157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0, 167.0, 168.0,
+            169.0, 170.0
+        )
+        CurrencyType.values().forEachIndexed { index, currencyType ->
+            assertEquals((index + 1).toDouble(), rates.getConversionByName(currencyType.toString()))
+        }
 
-        assertEquals(rates.eur, rates.getConversionByName("EUR"))
-        assertEquals(rates.usd, rates.getConversionByName("USD"))
+        assertEquals(0.0, rates.getConversionByName("some string"))
+        assertEquals(0.0, rates.getConversionByName(""))
     }
 
     @Test
