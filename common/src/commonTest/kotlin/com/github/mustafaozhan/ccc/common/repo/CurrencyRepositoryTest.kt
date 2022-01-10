21c7bec72320401775ef36d7e4e0ba14da49b857
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-01-10 15:53:33 +0300

    added test cases for unit Api methods

--- common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/repo/CurrencyRepositoryTest.kt
+++ common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/repo/CurrencyRepositoryTest.kt
@@ -3,16 +3,58 @@ package com.github.mustafaozhan.ccc.common.repo
 import com.github.mustafaozhan.ccc.common.db.currency.CurrencyRepository
 import com.github.mustafaozhan.ccc.common.db.currency.CurrencyRepositoryImpl
 import com.github.mustafaozhan.ccc.common.db.sql.CurrencyQueries
+import com.github.mustafaozhan.logmob.initLogger
+import io.mockative.ConfigurationApi
 import io.mockative.Mock
 import io.mockative.classOf
+import io.mockative.configure
+import io.mockative.eq
 import io.mockative.mock
+import io.mockative.verify
+import kotlin.random.Random
+import kotlin.test.BeforeTest
+import kotlin.test.Test
 
+@ConfigurationApi
 class CurrencyRepositoryTest {
 
     @Mock
-    private val currencyQueries = mock(classOf<CurrencyQueries>())
+    private val currencyQueries = configure(mock(classOf<CurrencyQueries>())) {
+        stubsUnitByDefault = true
+    }
 
     private val repository: CurrencyRepository by lazy {
         CurrencyRepositoryImpl(currencyQueries)
+    }
+
+    @BeforeTest
+    fun setup() {
+        initLogger(true)
+    }
+
+    @Test
+    fun updateCurrencyStateByName() {
+        val mockName = "mock"
+        val mockState = Random.nextBoolean()
+        val databaseValue = if (mockState) 1.toLong() else 0.toLong()
+        repository.updateCurrencyStateByName(mockName, mockState)
+
+        verify(currencyQueries)
+            .function(currencyQueries::updateCurrencyStateByName)
+            .with(eq(mockName), eq(databaseValue))
+            .wasInvoked()
+    }
+
+    @Test
+    fun updateAllCurrencyState() {
+        val mockState = Random.nextBoolean()
+        val databaseValue = if (mockState) 1.toLong() else 0.toLong()
+
+        repository.updateAllCurrencyState(mockState)
+
+        verify(currencyQueries)
+            .function(currencyQueries::updateAllCurrencyState)
+            .with(eq(databaseValue))
+            .wasInvoked()
     }
 }
--- common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/repo/OfflineRatesRepositoryTest.kt
+++ common/src/commonTest/kotlin/com/github/mustafaozhan/ccc/common/repo/OfflineRatesRepositoryTest.kt
@@ -5,22 +5,18 @@ import com.github.mustafaozhan.ccc.common.db.offlinerates.OfflineRatesRepository
 import com.github.mustafaozhan.ccc.common.db.sql.OfflineRatesQueries
 import com.github.mustafaozhan.ccc.common.entity.CurrencyResponseEntity
 import com.github.mustafaozhan.ccc.common.entity.RatesEntity
-import com.github.mustafaozhan.ccc.common.mapper.toCurrencyResponseEntity
 import com.github.mustafaozhan.ccc.common.mapper.toModel
 import com.github.mustafaozhan.ccc.common.mapper.toOfflineRates
-import com.github.mustafaozhan.ccc.common.mapper.toSerializedString
 import com.github.mustafaozhan.logmob.initLogger
 import io.mockative.ConfigurationApi
 import io.mockative.Mock
 import io.mockative.classOf
 import io.mockative.configure
 import io.mockative.eq
-import io.mockative.given
 import io.mockative.mock
 import io.mockative.verify
 import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 @ConfigurationApi
 class OfflineRatesRepositoryTest {
@@ -36,7 +32,6 @@ class OfflineRatesRepositoryTest {
 
     private val currencyResponseEntity = CurrencyResponseEntity("EUR", "12.21.2121", RatesEntity())
     private val currencyResponse = currencyResponseEntity.toModel()
-    private val offlineRates = currencyResponse.toOfflineRates()
 
     @BeforeTest
     fun setup() {
@@ -51,38 +46,5 @@ class OfflineRatesRepositoryTest {
             .function(offlineRatesQueries::insertOfflineRates)
             .with(eq(currencyResponse.toOfflineRates()))
             .wasInvoked()
-    }
-
-    @Test
-    fun getOfflineRatesByBase() {
-        given(offlineRatesQueries)
-            .invocation {
-                offlineRatesQueries.getOfflineRatesByBase(currencyResponse.base)
-                    .executeAsOneOrNull()
-            }
-            .thenReturn(offlineRates)
-
-        assertEquals(
-            currencyResponse.rates,
-            repository.getOfflineRatesByBase(currencyResponse.base)
-        )
-    }
-
-    @Test
-    fun getOfflineCurrencyResponseByBase() {
-        given(offlineRatesQueries)
-            .invocation {
-                offlineRatesQueries
-                    .getOfflineRatesByBase(currencyResponse.base.uppercase())
-                    .executeAsOneOrNull()
-                    ?.toCurrencyResponseEntity()
-                    ?.toSerializedString()
-            }
-            .thenReturn(currencyResponseEntity.toSerializedString())
-
-        assertEquals(
-            currencyResponseEntity.toSerializedString(),
-            repository.getOfflineCurrencyResponseByBase(currencyResponse.base)
-        )
     }
 }
