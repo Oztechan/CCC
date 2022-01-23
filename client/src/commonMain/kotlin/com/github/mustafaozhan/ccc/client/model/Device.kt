10763e3b6c0bde846872b6336c6af5bb911fe7c2
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-01-23 21:23:57 +0100

    [#407] add an empty line

--- client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/model/Device.kt
+++ client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/model/Device.kt
@@ -1,6 +1,7 @@
 package com.github.mustafaozhan.ccc.client.model
 
 sealed class Device(open val name: String) {
+
     sealed class ANDROID(override val name: String) : Device(name) {
         object GOOGLE : ANDROID("google")
         object HUAWEI : ANDROID("huawei")
