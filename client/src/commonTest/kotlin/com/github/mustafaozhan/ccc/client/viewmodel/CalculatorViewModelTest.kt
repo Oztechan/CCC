8998e78275e7314387cbeed3beebd7ed6ea237e8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-15 21:40:58 +0100

    [#457] remove remaining ConfigurationApis

--- client/src/commonTest/kotlin/com/github/mustafaozhan/ccc/client/viewmodel/CalculatorViewModelTest.kt
+++ client/src/commonTest/kotlin/com/github/mustafaozhan/ccc/client/viewmodel/CalculatorViewModelTest.kt
@@ -25,7 +25,6 @@ import com.github.mustafaozhan.logmob.initLogger
 import io.mockative.Mock
 import io.mockative.any
 import io.mockative.classOf
-import io.mockative.configure
 import io.mockative.given
 import io.mockative.mock
 import io.mockative.verify
@@ -36,13 +35,10 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
-@ConfigurationApi
 class CalculatorViewModelTest {
 
     @Mock
-    private val settingsRepository = configure(mock(classOf<SettingsRepository>())) {
-        stubsUnitByDefault = true
-    }
+    private val settingsRepository = mock(classOf<SettingsRepository>())
 
     @Mock
     private val apiRepository = mock(classOf<ApiRepository>())
@@ -51,9 +47,7 @@ class CalculatorViewModelTest {
     private val currencyRepository = mock(classOf<CurrencyRepository>())
 
     @Mock
-    private val offlineRatesRepository = configure(mock(classOf<OfflineRatesRepository>())) {
-        stubsUnitByDefault = true
-    }
+    private val offlineRatesRepository = mock(classOf<OfflineRatesRepository>())
 
     @Mock
     private val sessionManager = mock(classOf<SessionManager>())
