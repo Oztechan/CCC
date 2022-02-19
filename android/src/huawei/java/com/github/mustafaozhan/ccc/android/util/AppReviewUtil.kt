51da998288cd6b0d6c36cafa7b789de730765f0a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-19 13:44:03 +0100

    [#465] move AppReview in variant specific folders

---
+++ android/src/huawei/java/com/github/mustafaozhan/ccc/android/util/AppReviewUtil.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 Mustafa Ozhan. All rights reserved.
+ */
+package com.github.mustafaozhan.ccc.android.util
+
+import android.app.Activity
+import com.google.android.play.core.review.ReviewManagerFactory
+
+fun requestAppReview(activity: Activity) = Unit
