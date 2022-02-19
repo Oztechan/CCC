2d38bdc692db2dde6face26a1d6f68754952053f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-19 22:03:57 +0100

    [#465] change the order of variables

--- config/src/commonMain/kotlin/com/github/mustafaozhan/config/model/AppConfig.kt
+++ config/src/commonMain/kotlin/com/github/mustafaozhan/config/model/AppConfig.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AppConfig(
+    @SerialName("timeout_anr_watch_dog") val timeOutANRWatchDog: Int = 7500,
     @SerialName("ad_config") val adConfig: AdConfig = AdConfig(),
-    @SerialName("app_update") val appUpdate: List<AppUpdate> = listOf(),
     @SerialName("app_review") val appReview: AppReview = AppReview(),
-    @SerialName("timeout_anr_watch_dog") val timeOutANRWatchDog: Int = 7500
+    @SerialName("app_update") val appUpdate: List<AppUpdate> = listOf()
 )
