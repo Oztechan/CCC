a32e07c0357caefdf9b686aea6cb3f16f4f8f9c6
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-02-20 17:45:59 +0100

    [#445] put methods to one line

--- client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/model/AppTheme.kt
+++ client/src/commonMain/kotlin/com/github/mustafaozhan/ccc/client/model/AppTheme.kt
@@ -13,15 +13,11 @@ enum class AppTheme(val order: Int, val typeName: String, val themeValue: Int) {
         // dark theme applies below SDK 29
         const val SYSTEM_DARK = "System dark"
 
-        fun getThemeByValue(value: Int) = values().firstOrNull {
-            it.themeValue == value
-        }
+        fun getThemeByValue(value: Int) = values().firstOrNull { it.themeValue == value }
 
         fun getThemeByValueOrDefault(value: Int) = getThemeByValue(value) ?: SYSTEM_DEFAULT
 
-        fun getThemeByOrder(order: Int) = values().firstOrNull {
-            it.order == order
-        }
+        fun getThemeByOrder(order: Int) = values().firstOrNull { it.order == order }
 
         fun getThemeByOrderOrDefault(order: Int) = getThemeByOrder(order) ?: SYSTEM_DEFAULT
     }
