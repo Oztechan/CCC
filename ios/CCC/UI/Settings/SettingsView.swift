93418359bb908a054f0ec1e0c806e99f08536c4c
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-07-17 00:24:28 +0300

    [#833] Simplify iOS DI

--- ios/CCC/UI/Settings/SettingsView.swift
+++ ios/CCC/UI/Settings/SettingsView.swift
@@ -18,7 +18,7 @@ typealias SettingsObservable = ObservableSEED
 struct SettingsView: View {
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject private var navigationStack: NavigationStack
-    @StateObject var observable: SettingsObservable = koin.get()
+    @StateObject var observable = SettingsObservable(viewModel: koin.get())
     @State var dialogVisibility: Bool = false
     @State var emailViewVisibility: Bool = false
     @State var webViewVisibility: Bool = false
