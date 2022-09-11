e6e76c784e5fc39f58f643611b7a247cf4107087
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-09-11 13:39:59 +0200

    [#1049] Add analytics test cases (#1050)

---
+++ analytics/src/commonTest/kotlin/com/oztechan/ccc/analytics/model/ScreenNameTest.kt
@@ -0,0 +1,50 @@
+package com.oztechan.ccc.analytics.model
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ScreenNameTest {
+    @Test
+    fun `screenName Calculator return correct name`() {
+        val screenName = ScreenName.Calculator
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName SelectCurrency return correct name`() {
+        val screenName = ScreenName.SelectCurrency
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName Currencies return correct name`() {
+        val screenName = ScreenName.Currencies
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName Settings return correct name`() {
+        val screenName = ScreenName.Settings
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName Watchers return correct name`() {
+        val screenName = ScreenName.Watchers
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName AdRemove return correct name`() {
+        val screenName = ScreenName.AdRemove
+        assertEquals(screenName::class.simpleName.toString(), screenName.getScreenName())
+    }
+
+    @Test
+    fun `screenName Slider return correct name`() {
+        val position = Random.nextInt()
+        val screenName = ScreenName.Slider(position)
+        assertEquals("${screenName::class.simpleName} $position", screenName.getScreenName())
+    }
+}
