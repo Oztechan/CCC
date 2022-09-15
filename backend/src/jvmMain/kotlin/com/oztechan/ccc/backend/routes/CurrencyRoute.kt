33034a225d8811b877443c99003fa2c6e9b067b0
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-09-15 15:38:43 +0200

    [#1069] Replace backend controllers with repository

--- backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/routes/CurrencyRoute.kt
+++ backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/routes/CurrencyRoute.kt
@@ -5,7 +5,7 @@
 package com.oztechan.ccc.backend.routes
 
 import co.touchlab.kermit.Logger
-import com.oztechan.ccc.backend.controller.RootingController
+import com.oztechan.ccc.backend.repository.api.ApiRepository
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
@@ -14,10 +14,10 @@ import io.ktor.server.routing.get
 private const val PATH_BY_BASE = "/currency/byBase/"
 private const val PARAMETER_BASE = "base"
 
-suspend fun Route.getCurrencyByName(rootingController: RootingController) = get(PATH_BY_BASE) {
+suspend fun Route.getCurrencyByName(apiController: ApiRepository) = get(PATH_BY_BASE) {
     call.parameters[PARAMETER_BASE]?.let { base ->
         Logger.i { "GET Request $PARAMETER_BASE $base" }
-        rootingController.getOfflineCurrencyResponseByBase(base)?.let {
+        apiController.getOfflineCurrencyResponseByBase(base)?.let {
             call.respond(it)
         }
     } ?: run {
