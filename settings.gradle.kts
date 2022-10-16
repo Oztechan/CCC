378ca222065167f4f7f5bcc0d7ce51601e3138f7
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-16 17:22:36 +0300

    [#1218] reorganize settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,32 +3,32 @@
  */
 
 include(
-    ":android",
-    ":billing",
-    ":ad",
-    ":analytics"
-)
+    // Targets
+    ":android", // android app
+    ":backend", // backend app
+    // ios -> not an gradle module
 
-include(":backend")
-
-// KMP
-include(
-    ":client", // Shared with all FE targets
-    ":res", // Shared with all FE targets
-    ":config", // Shared with all FE targets
+    // KMP modules
     ":common", // Shared with all FE & BE targets
-    ":test"
-)
+    ":client", // Shared with all FE targets
 
-// native
-include(":provider") // used for combining iOS libraries and injecting as pod
+    ":res", // Shared with all FE targets for resources
 
-// submodules
-include(
-    ":logmob",
-    ":scopemob",
-    ":basemob",
-    ":parsermob"
+    ":config", // Shared with all FE targets for Firebase Remote Config
+    ":analytics", // Shared with all FE targets for Google Analytics
+
+    ":test", // common test classes
+
+    ":provider", // umbrella framework for iOS libraries
+
+    ":billing", // android only billing module
+    ":ad", // android only ad module
+
+    // submodules
+    ":logmob", // KMP, logger library
+    ":scopemob", // KMP, hand scope functions
+    ":basemob", // android only
+    ":parsermob" // KMP, parsing library
 )
 
 project(":logmob").projectDir = file("logmob/logmob")
