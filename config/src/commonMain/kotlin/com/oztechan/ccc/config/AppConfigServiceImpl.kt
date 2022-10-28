633c6fac50af708a653d825c09e4877abc050281
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-10-28 15:34:05 +0300

    [#1262] Create BaseConfigService (#1263)

--- config/src/commonMain/kotlin/com/oztechan/ccc/config/AppConfigServiceImpl.kt
+++ config/src/commonMain/kotlin/com/oztechan/ccc/config/AppConfigServiceImpl.kt
@@ -1,7 +1,23 @@
 package com.oztechan.ccc.config
 
-import com.oztechan.ccc.config.model.AppConfig
+import com.oztechan.ccc.config.mapper.toModel
+import kotlinx.serialization.decodeFromString
+import com.oztechan.ccc.config.entity.AppConfig as AppConfigEntity
+import com.oztechan.ccc.config.model.AppConfig as AppConfigModel
 
-internal expect class AppConfigServiceImpl() : AppConfigService {
-    override var appConfig: AppConfig
+internal class AppConfigServiceImpl :
+    BaseConfigService<AppConfigModel>(
+        KEY_AD_CONFIG,
+        AppConfigEntity().toModel()
+    ),
+    AppConfigService {
+
+    override fun decode(
+        value: String
+    ) = json.decodeFromString<AppConfigEntity>(value)
+        .toModel()
+
+    companion object {
+        private const val KEY_AD_CONFIG = "app_config"
+    }
 }
