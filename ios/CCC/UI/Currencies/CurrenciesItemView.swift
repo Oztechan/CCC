ae75da2b8e46234858b91e33a63eb825f62432ee
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-11-15 12:18:38 +0300

    [Oztechan/CCC#1319] Add iPad friendly Currencies Screen

--- ios/CCC/UI/Currencies/CurrenciesItemView.swift
+++ ios/CCC/UI/Currencies/CurrenciesItemView.swift
@@ -23,20 +23,27 @@ struct CurrenciesItemView: View {
             CurrencyImageView(imageName: item.name)
 
             Text(item.name)
-                .frame(width: 45)
+                .font(relative: .footnote)
                 .foregroundColor(MR.colors().text.get())
+
             Text(item.longName)
                 .font(relative: .footnote)
                 .foregroundColor(MR.colors().text.get())
+
             Text(item.symbol)
                 .font(relative: .footnote)
                 .foregroundColor(MR.colors().text.get())
+
             Spacer()
+
             Image(systemName: item.isActive ? "checkmark.circle.fill" : "circle")
+                .resizable()
+                .frame(width: 20.cp(), height: 20.cp(), alignment: .center)
                 .foregroundColor(MR.colors().accent.get())
 
         }
         .contentShape(Rectangle())
+        .padding(.vertical, 4.cp())
         .onTapGesture { onItemClick() }
         .onLongPressGesture { onItemLongClick() }
         .lineLimit(1)
