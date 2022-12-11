44c936e5e6bdbd7857ea6199ecac203a5dff918f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-12-11 16:47:25 +0200

    [Oztechan/CCC#1422] Extract ClientController

---
+++ backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/controller/client/ClientController.kt
@@ -0,0 +1,5 @@
+package com.oztechan.ccc.backend.controller.client
+
+interface ClientController {
+    fun startSyncApi()
+}
--- backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/di/module/submodule/ControllerModule.kt
+++ backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/di/module/submodule/ControllerModule.kt
@@ -1,9 +1,14 @@
 package com.oztechan.ccc.backend.di.module.submodule
 
+import com.oztechan.ccc.backend.controller.client.ClientController
+import com.oztechan.ccc.backend.controller.client.ClientControllerImpl
 import com.oztechan.ccc.backend.controller.server.ServerController
 import com.oztechan.ccc.backend.controller.server.ServerControllerImpl
+import com.oztechan.ccc.common.di.DISPATCHER_IO
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 internal val controllerModule = module {
+    single<ClientController> { ClientControllerImpl(get(), get(), get(), get(), get(named(DISPATCHER_IO))) }
     single<ServerController> { ServerControllerImpl(get()) }
 }
