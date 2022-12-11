35c75a85a5f6adeba3d070c57f2ce664b372d8d3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-12-11 15:49:17 +0200

    [Oztechan/CCC#1419] Add server and client module and application.conf for backend

---
+++ backend/src/jvmMain/kotlin/com/oztechan/ccc/backend/module/Client.kt
@@ -0,0 +1,14 @@
+package com.oztechan.ccc.backend.module
+
+import co.touchlab.kermit.Logger
+import com.oztechan.ccc.backend.repository.api.ApiRepository
+import io.ktor.server.application.Application
+import org.koin.java.KoinJavaComponent.inject
+
+@Suppress("unused", "UnusedReceiverParameter")
+internal fun Application.client() {
+    val apiController: ApiRepository by inject(ApiRepository::class.java)
+
+    Logger.i { "start sync" }
+    apiController.startSyncApi()
+}
