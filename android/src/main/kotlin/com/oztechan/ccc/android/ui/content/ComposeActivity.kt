d68ef586294dd99a84f3396107c7b7f47b2d48d7
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-12-22 15:16:15 +0200

    [Oztechan/CCC#1512] Add ComposeActivity and show only for DEBUG mode

---
+++ android/src/main/kotlin/com/oztechan/ccc/android/ui/content/ComposeActivity.kt
@@ -0,0 +1,27 @@
+package com.oztechan.ccc.android.ui.content
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.oztechan.ccc.android.ui.theme.AppTheme
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Hello World!")
+                }
+            }
+        }
+    }
+}
