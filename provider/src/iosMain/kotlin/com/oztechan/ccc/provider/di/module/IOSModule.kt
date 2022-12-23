6cd902274ef8ecf7c3ee03762709da6b382d37ca
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2022-12-23 12:21:29 +0200

    [Oztechan/CCC#1524] Remove NativeDependencyWrapper

--- provider/src/iosMain/kotlin/com/oztechan/ccc/provider/di/module/IOSModule.kt
+++ provider/src/iosMain/kotlin/com/oztechan/ccc/provider/di/module/IOSModule.kt
@@ -1,9 +1,8 @@
 package com.oztechan.ccc.provider.di.module
 
-import com.oztechan.ccc.client.di.NativeDependencyWrapper
 import org.koin.dsl.module
 import platform.Foundation.NSUserDefaults
 
 internal fun getIOSModule(userDefaults: NSUserDefaults) = module {
-    single { NativeDependencyWrapper(userDefaults) }
+    single { userDefaults }
 }
