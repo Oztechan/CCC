36869860072873e7e507f2010a4e07096dd6956d
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-01-06 18:02:32 +0200

    [Oztechan/CCC#1622] Exclude Composable and its Previews from coverage reports (#1623)
    * [Oztechan/CCC#1622] Exclude Composable and its Previews from coverage reports
    
    * [Oztechan/CCC#1622] Exclude Composable and its Previews from coverage reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,18 @@ version = ProjectSettings.getVersionName(project)
 
 allprojects {
     apply(plugin = rootProject.libs.plugins.kover.get().pluginId).also {
-        koverMerged.enable()
+        koverMerged {
+            filters {
+                annotations {
+                    excludes += listOf(
+                        "com.oztechan.ccc.android.ui.compose.annotations.ThemedPreviews",
+                        "androidx.compose.ui.tooling.preview.Preview",
+                        "androidx.compose.runtime.Composable"
+                    )
+                }
+            }
+            enable()
+        }
     }
 
     apply(plugin = rootProject.libs.plugins.detekt.get().pluginId).also {
