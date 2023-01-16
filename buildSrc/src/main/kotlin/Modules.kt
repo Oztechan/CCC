e6f765e5e668b9e3f58cf6bf1955af9f6fd12355
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-01-16 19:09:03 +0200

    [Oztechan/CCC#1705] Create Submodules structure for Git submodules

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,23 +1,23 @@
 object Modules {
-    val android = ":android"
-    val ios = ":ios"
-    val backend = ":backend"
+    const val android = ":android"
+    const val ios = ":ios"
+    const val backend = ":backend"
 
-    val client = ":client"
-    val res = ":res"
-    val common = ":common"
-    val billing = ":billing"
-    val ad = ":ad"
-    val analytics = ":analytics"
-    val config = ":config"
-    val test = ":test"
-    val provider = ":provider"
+    const val client = ":client"
+    const val res = ":res"
+    const val common = ":common"
+    const val billing = ":billing"
+    const val ad = ":ad"
+    const val analytics = ":analytics"
+    const val config = ":config"
+    const val test = ":test"
+    const val provider = ":provider"
 
     object Submodules {
-        val logmob = ":submodule:logmob"
-        val scopemob = ":submodule:scopemob"
-        val basemob = ":submodule:basemob"
-        val parsermob = ":submodule:parsermob"
+        const val logmob = ":submodule:logmob"
+        const val scopemob = ":submodule:scopemob"
+        const val basemob = ":submodule:basemob"
+        const val parsermob = ":submodule:parsermob"
     }
 }
 
