19a47b5c06d51bad2016bb8c3b6b024abba489f5
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-01-22 11:17:49 +0200

    [Oztechan/CCC#1770] Create ExchangeRateDataSource for backend specific operations

--- buildSrc/src/main/kotlin/SecretExt.kt
+++ buildSrc/src/main/kotlin/SecretExt.kt
@@ -2,6 +2,8 @@ import org.gradle.api.Project
 import java.io.IOException
 import java.util.Properties
 
+private const val PATH_SECRET_PROPERTIES = "../secret.properties"
+
 fun Project.getSecret(
     key: String,
     default: String = "secret" // these values can not be public
@@ -14,11 +16,7 @@ fun Project.getSecret(
 }
 
 private fun Project.getSecretProperties() = try {
-    Properties().apply {
-        load(
-            file("${rootProject.rootDir}/secret.properties").inputStream()
-        )
-    }
+    Properties().apply { load(file(PATH_SECRET_PROPERTIES).inputStream()) }
 } catch (e: IOException) {
     logger.debug(e.message, e)
     null
