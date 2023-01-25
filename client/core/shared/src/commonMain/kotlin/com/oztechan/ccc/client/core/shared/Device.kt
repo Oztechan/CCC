b07ebefe83362dd040ca241199bf482b54de059a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-01-25 16:08:37 +0200

    [Oztechan/CCC#1813] Create :client:core:infrastructure module

--- client/core/shared/src/commonMain/kotlin/com/oztechan/ccc/client/core/shared/Device.kt
+++ client/core/shared/src/commonMain/kotlin/com/oztechan/ccc/client/core/shared/Device.kt
@@ -1,4 +1,10 @@
+<<<<<<<< HEAD:client/core/shared/src/commonMain/kotlin/com/oztechan/ccc/client/core/shared/Device.kt
 package com.oztechan.ccc.client.core.shared
+|||||||| parent of 001c3b853 ([Oztechan/CCC#1813] Create :client:core:infrastructure module):client/src/commonMain/kotlin/com/oztechan/ccc/client/model/Device.kt
+package com.oztechan.ccc.client.model
+========
+package com.oztechan.ccc.client.core.infrastructure
+>>>>>>>> 001c3b853 ([Oztechan/CCC#1813] Create :client:core:infrastructure module):client/core/infrastructure/src/commonMain/kotlin/com/oztechan/ccc/client/core/infrastructure/Device.kt
 
 sealed class Device(
     open val name: String,
