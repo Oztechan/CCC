d720cc1878221b0c8bfb4e463325ffe3d18b8bb8
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-02-12 22:38:04 +0200

    [Oztechan/CCC#1992] Map system widget update action to WidgetActions and prevent unnecessary refresh (#1993)

--- android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/action/WidgetAction.kt
+++ android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/action/WidgetAction.kt
@@ -1,5 +1,6 @@
 package com.oztechan.ccc.android.ui.widget.action
 
+import android.appwidget.AppWidgetManager
 import androidx.glance.action.ActionParameters
 import androidx.glance.action.actionParametersOf
 import androidx.glance.appwidget.action.actionRunCallback
@@ -8,13 +9,27 @@ enum class WidgetAction {
     OPEN_APP,
     NEXT_BASE,
     PREVIOUS_BASE,
-    REFRESH;
+    REFRESH,
+    IDLE;
 
     companion object {
-        fun String?.getWidgetActionOrNull() = this?.let {
-            WidgetAction.values().firstOrNull { widgetAction ->
-                it == widgetAction.name
-            }
+        fun String?.mapToWidgetAction() = when (this) {
+            // no needed to refresh
+            AppWidgetManager.ACTION_APPWIDGET_DISABLED,
+            AppWidgetManager.ACTION_APPWIDGET_DELETED,
+            AppWidgetManager.ACTION_APPWIDGET_ENABLED -> IDLE
+
+            // unknown ones perhaps no need to refresh (yet)
+            AppWidgetManager.ACTION_APPWIDGET_BIND,
+            AppWidgetManager.ACTION_APPWIDGET_PICK,
+            AppWidgetManager.ACTION_APPWIDGET_CONFIGURE,
+            AppWidgetManager.ACTION_APPWIDGET_RESTORED,
+            AppWidgetManager.ACTION_APPWIDGET_HOST_RESTORED -> IDLE
+
+            AppWidgetManager.ACTION_APPWIDGET_UPDATE,
+            AppWidgetManager.ACTION_APPWIDGET_OPTIONS_CHANGED -> REFRESH
+
+            else -> WidgetAction.values().firstOrNull { this == it.name }
         }
 
         fun WidgetAction.toActionCallback() = actionRunCallback<WidgetActionCallback>(
