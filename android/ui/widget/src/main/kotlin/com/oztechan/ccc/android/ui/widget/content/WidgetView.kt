d703f170f2c3fb005d64b5103cba081e58b42192
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-02-19 09:07:51 +0200

    [Oztechan/CCC#2050] Create Resources for widget items (#2051)

--- android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/content/WidgetView.kt
+++ android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/content/WidgetView.kt
@@ -3,6 +3,7 @@ package com.oztechan.ccc.android.ui.widget.content
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalContext
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
@@ -12,6 +13,7 @@ import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
 import com.oztechan.ccc.android.ui.widget.R
 import com.oztechan.ccc.android.viewmodel.widget.WidgetState
 
@@ -34,9 +36,12 @@ fun WidgetView(state: WidgetState) {
             Spacer(modifier = GlanceModifier.defaultWeight())
 
             Text(
-                text = "Widget is only available in Premium",
+                text = LocalContext.current.getString(R.string.txt_widget_available_only_premium),
                 modifier = GlanceModifier.padding(8.dp),
-                style = TextStyle(textAlign = TextAlign.Center)
+                style = TextStyle(
+                    color = ColorProvider(R.color.text),
+                    textAlign = TextAlign.Center
+                )
             )
         }
 
