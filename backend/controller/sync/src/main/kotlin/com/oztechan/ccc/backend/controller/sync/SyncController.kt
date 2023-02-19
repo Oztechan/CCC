cb0d1a3532dca0fd0ae00eb669030357b8d1afff
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-02-19 10:57:47 +0200

    [Oztechan/CCC#2063] Create primary, secondary and tertiary currencies to fetch them according to usage

--- backend/controller/sync/src/main/kotlin/com/oztechan/ccc/backend/controller/sync/SyncController.kt
+++ backend/controller/sync/src/main/kotlin/com/oztechan/ccc/backend/controller/sync/SyncController.kt
@@ -1,6 +1,8 @@
 package com.oztechan.ccc.backend.controller.sync
 
 interface SyncController {
-    suspend fun syncPopularCurrencies()
+    suspend fun syncPrimaryCurrencies()
+    suspend fun syncSecondaryCurrencies()
+    suspend fun syncTertiaryCurrencies()
     suspend fun syncUnPopularCurrencies()
 }
