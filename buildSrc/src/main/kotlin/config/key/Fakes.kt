fd4f11157fb075639c3c67e7ad4bd41dbc405b12
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-02-22 00:21:11 +0200

    [Oztechan/CCC#2088] Create Key for single usage secrets

--- buildSrc/src/main/kotlin/config/key/Fakes.kt
+++ buildSrc/src/main/kotlin/config/key/Fakes.kt
@@ -4,7 +4,6 @@ object Fakes {
     const val PRIVATE_URL = "http://www.private-url.com"
     const val PRIVATE_PATH = "private/path"
 
-
     object Google {
         const val ADVERTISEMENT_ID = "ca-app-pub-3940256099942544~3347511713"
         const val BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111"
