3605b6a2dd4130d8cf9f66fda22a08746d3027b0
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-02-26 14:11:46 +0200

    [Oztechan/CCC#2121] Make :viewmodel:premium KMM module

--- client/viewmodel/premium/client-viewmodel-premium.gradle.kts
+++ client/viewmodel/premium/client-viewmodel-premium.gradle.kts
@@ -1,10 +1,86 @@
 plugins {
     @Suppress("DSL_SCOPE_VIOLATION")
     libs.plugins.apply {
+        id(multiplatform.get().pluginId)
         id(androidLib.get().pluginId)
-        id(android.get().pluginId)
         alias(ksp)
     }
+}
+kotlin {
+    android()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    @Suppress("UNUSED_VARIABLE")
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                libs.common.apply {
+                    implementation(koinCore)
+                    implementation(kermit)
+                    implementation(coroutines)
+                }
+
+                Modules.Client.Core.apply {
+                    implementation(project(viewModel))
+                    implementation(project(shared))
+                }
+
+                Modules.Client.Storage.apply {
+                    implementation(project(app))
+                }
+
+                Modules.Submodules.apply {
+                    implementation(project(scopemob))
+                }
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                libs.common.apply {
+                    implementation(test)
+                    implementation(mockative)
+                    implementation(coroutinesTest)
+                }
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.android.lifecycleViewmodel)
+            }
+        }
+        val androidUnitTest by getting
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+dependencies {
+    configurations
+        .filter { it.name.startsWith("ksp") && it.name.contains("Test") }
+        .forEach {
+            add(it.name, libs.processors.mockative)
+        }
 }
 
 android {
@@ -17,33 +93,5 @@ android {
             sourceCompatibility = JAVA_VERSION
             targetCompatibility = JAVA_VERSION
         }
-    }
-}
-
-dependencies {
-    libs.common.apply {
-        implementation(koinCore)
-        implementation(coroutines)
-        implementation(kermit)
-        implementation(libs.android.lifecycleViewmodel)
-
-        testImplementation(test)
-        testImplementation(mockative)
-        testImplementation(coroutinesTest)
-    }
-
-    kspTest(libs.processors.mockative)
-
-    Modules.Client.Core.apply {
-        implementation(project(viewModel))
-        implementation(project(shared))
-    }
-
-    Modules.Client.Storage.apply {
-        implementation(project(app))
-    }
-
-    Modules.Submodules.apply {
-        implementation(project(scopemob))
     }
 }
