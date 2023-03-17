75223f4fdea1461d48b6b9cf2f8428e8d92130d3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-03-17 10:07:48 +0100

    [Oztechan/CCC#2208] Separate TooBigNumber to TooBigInput and TooBigOutput

--- android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/calculator/CalculatorFragment.kt
+++ android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/calculator/CalculatorFragment.kt
@@ -152,6 +152,8 @@ class CalculatorFragment : BaseVBFragment<FragmentCalculatorBinding>() {
                     viewEffect.text,
                     icon = viewEffect.code.getImageIdByName()
                 )
+
+                else -> TODO("will be removed")
             }
         }.launchIn(viewLifecycleOwner.lifecycleScope)
 
