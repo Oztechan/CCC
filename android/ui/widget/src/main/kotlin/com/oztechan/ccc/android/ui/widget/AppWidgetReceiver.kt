99e39bdbf722b0984711437e86d07f9ed95caf73
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-04-03 18:37:38 +0200

    [Oztechan/CCC#2251] Extract WidgetActions and SystemActions into Receiver

--- android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/AppWidgetReceiver.kt
+++ android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/AppWidgetReceiver.kt
@@ -41,25 +41,47 @@ class AppWidgetReceiver : GlanceAppWidgetReceiver(), KoinComponent {
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
 
-        when (intent.action.mapToWidgetAction()) {
-            WidgetAction.IDLE -> Unit
-            WidgetAction.REFRESH -> refreshData(context)
-            WidgetAction.NEXT_BASE -> refreshData(context, true)
-            WidgetAction.PREVIOUS_BASE -> refreshData(context, false)
-            WidgetAction.OPEN_APP ->
-                context.packageManager
-                    .getLaunchIntentForPackage(context.packageName)
-                    ?.apply {
-                        addFlags(
-                            Intent.FLAG_ACTIVITY_CLEAR_TASK or
-                                Intent.FLAG_ACTIVITY_CLEAR_TOP or
-                                Intent.FLAG_ACTIVITY_NEW_TASK
-                        )
-                    }?.let {
-                        context.startActivity(it)
-                    }
-
-            else -> error("undefined widget action")
+        intent.action.let {
+            it.mapToWidgetAction()?.executeWidgetAction(context)
+                ?: it.executeSystemAction(context)
         }
+    }
+
+    private fun WidgetAction.executeWidgetAction(context: Context) = when (this) {
+        WidgetAction.IDLE -> Unit
+        WidgetAction.REFRESH -> refreshData(context)
+        WidgetAction.NEXT_BASE -> refreshData(context, true)
+        WidgetAction.PREVIOUS_BASE -> refreshData(context, false)
+        WidgetAction.OPEN_APP ->
+            context.packageManager
+                .getLaunchIntentForPackage(context.packageName)
+                ?.apply {
+                    addFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                            Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                            Intent.FLAG_ACTIVITY_NEW_TASK
+                    )
+                }?.let {
+                    context.startActivity(it)
+                }
+    }
+
+    private fun String?.executeSystemAction(context: Context) = when (this) {
+        // no needed to refresh
+        AppWidgetManager.ACTION_APPWIDGET_DISABLED,
+        AppWidgetManager.ACTION_APPWIDGET_DELETED,
+        AppWidgetManager.ACTION_APPWIDGET_ENABLED -> Unit
+
+        // unknown ones perhaps no need to refresh (yet)
+        AppWidgetManager.ACTION_APPWIDGET_BIND,
+        AppWidgetManager.ACTION_APPWIDGET_PICK,
+        AppWidgetManager.ACTION_APPWIDGET_CONFIGURE,
+        AppWidgetManager.ACTION_APPWIDGET_RESTORED,
+        AppWidgetManager.ACTION_APPWIDGET_HOST_RESTORED -> Unit
+
+        AppWidgetManager.ACTION_APPWIDGET_UPDATE,
+        AppWidgetManager.ACTION_APPWIDGET_OPTIONS_CHANGED -> refreshData(context)
+
+        else -> error("undefined widget action")
     }
 }
