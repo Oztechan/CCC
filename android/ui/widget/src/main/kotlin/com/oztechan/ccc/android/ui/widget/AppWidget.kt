b8efc6ca7bb35a56a136cb080c9b104e3eb55a45
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-05-19 20:28:45 +0200

    [Oztechan/CCC#1457] Dependency update dependency androidx.glance:glance-appwidget to v1.0.0-beta01

--- android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/AppWidget.kt
+++ android/ui/widget/src/main/kotlin/com/oztechan/ccc/android/ui/widget/AppWidget.kt
@@ -1,7 +1,9 @@
 package com.oztechan.ccc.android.ui.widget
 
-import androidx.compose.runtime.Composable
+import android.content.Context
+import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
 import com.oztechan.ccc.android.ui.widget.content.WidgetView
 import com.oztechan.ccc.android.viewmodel.widget.WidgetViewModel
 import org.koin.core.component.KoinComponent
@@ -11,8 +13,9 @@ class AppWidget : GlanceAppWidget(), KoinComponent {
 
     private val viewModel: WidgetViewModel by inject()
 
-    @Composable
-    override fun Content() {
-        WidgetView(state = viewModel.state)
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            WidgetView(state = viewModel.state)
+        }
     }
 }
