edd13c0c91e6472f17136bcfda41f373778399cf
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-07-24 13:20:27 +0200

    [Oztechan/CCC#2511] Remove exception message checking test cases (#2512)
    * [Oztechan/CCC#2511] Remove exception message checking test cases
    
    * [Oztechan/CCC#2511] Remove exception message checking test cases

--- common/core/network/src/commonTest/kotlin/com/oztechan/ccc/common/core/network/base/BaseNetworkServiceTest.kt
+++ common/core/network/src/commonTest/kotlin/com/oztechan/ccc/common/core/network/base/BaseNetworkServiceTest.kt
@@ -13,9 +13,7 @@ import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.SerializationException
 import kotlin.coroutines.cancellation.CancellationException
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.assertNotNull
 
 @Suppress("OPT_IN_USAGE")
 class BaseNetworkServiceTest {
@@ -33,11 +31,6 @@ class BaseNetworkServiceTest {
     fun `CancellationException should return exception itself`() = runTest {
         assertFailsWith(TerminationException::class) {
             subject.request { throw CancellationException(exception) }
-        }.let {
-            assertNotNull(it.cause)
-            assertNotNull(it.cause.cause)
-            assertEquals(exception, it.cause.cause)
-            assertEquals(exception.message, it.cause.cause!!.message)
         }
     }
 
@@ -45,9 +38,6 @@ class BaseNetworkServiceTest {
     fun `IOException should return NetworkException`() = runTest {
         assertFailsWith(NetworkException::class) {
             subject.request { throw IOException(exception.message.toString()) }
-        }.let {
-            assertNotNull(it.cause)
-            assertEquals(exception.message, it.cause.message)
         }
     }
 
@@ -55,9 +45,6 @@ class BaseNetworkServiceTest {
     fun `ConnectTimeoutException should return TimeoutException`() = runTest {
         assertFailsWith(TimeoutException::class) {
             subject.request { throw ConnectTimeoutException(exception.message.toString()) }
-        }.let {
-            assertNotNull(it.cause)
-            assertEquals(exception.message, it.cause.message)
         }
     }
 
@@ -65,34 +52,20 @@ class BaseNetworkServiceTest {
     fun `SerializationException should return ModelMappingException`() = runTest {
         assertFailsWith(ModelMappingException::class) {
             subject.request { throw SerializationException(exception) }
-        }.let {
-            assertNotNull(it.cause)
-            assertNotNull(it.cause.cause)
-            assertEquals(exception, it.cause.cause)
-            assertEquals(exception.message, it.cause.cause!!.message)
         }
     }
 
-    @Suppress("TooGenericExceptionThrown")
     @Test
     fun `Any other exception should return UnknownNetworkException`() = runTest {
         assertFailsWith(UnknownNetworkException::class) {
-            subject.request { throw Exception(exception) }
-        }.let {
-            assertNotNull(it.cause)
-            assertNotNull(it.cause.cause)
-            assertEquals(exception, it.cause.cause)
-            assertEquals(exception.message, it.cause.cause!!.message)
+            subject.request { throw exception }
         }
     }
 
     @Test
-    fun `Empty string should return EmptyParameterException`() = runTest {
+    fun `Empty string should return EmptyParameterException`() {
         assertFailsWith(EmptyParameterException::class) {
             subject.parameterCheck("")
-        }.let {
-            assertNotNull(it)
-            assertEquals(EmptyParameterException().message, it.message)
         }
     }
 }
