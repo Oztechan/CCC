6542ff67c1a4c211abdecfc4663da5d30e099ed3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-07-29 21:38:15 +0200

    [Oztechan/CCC#2533] Add test cases for Parser

---
+++ client/core/remoteconfig/src/commonTest/kotlin/com/oztechan/ccc/client/core/remoteconfig/util/ParserTest.kt
@@ -0,0 +1,25 @@
+package com.oztechan.ccc.client.core.remoteconfig.util
+
+import co.touchlab.kermit.CommonWriter
+import co.touchlab.kermit.Logger
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNull
+
+class ParserTest {
+
+    @BeforeTest
+    fun setup() {
+        Logger.setLogWriters(CommonWriter())
+    }
+
+    @Test
+    fun `parseToObject returns null when invoked with null`() {
+        assertNull(null.parseToObject())
+    }
+
+    @Test
+    fun `parseToObject returns null when invoked with empty String`() {
+        assertNull("".parseToObject())
+    }
+}
