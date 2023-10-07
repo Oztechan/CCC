d0dd0395ee2329c2792d26e82d92d4f8bd49cba6
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-10-07 14:05:46 +0200

    [Oztechan/CCC#2821] Remove FreeAPI and its Service

--- backend/service/free/src/main/kotlin/com/oztechan/ccc/backend/service/free/FreeApiService.kt
+++
@@ -1,7 +0,0 @@
-package com.oztechan.ccc.backend.service.free
-
-import com.oztechan.ccc.common.core.model.Conversion
-
-interface FreeApiService {
-    suspend fun getConversion(base: String): Conversion
-}
