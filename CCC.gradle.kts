699264ccdf25ac034fc8d71df0af4bef389fa4b7
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2023-10-24 15:00:25 +0200

    [Oztechan/CCC#2901] Move org.jetbrains.kotlin.jvm to alias (#2902)

--- CCC.gradle.kts
+++ CCC.gradle.kts
@@ -7,6 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     libs.plugins.apply {
         alias(kotlinMultiplatform).apply(false)
+        alias(kotlinJvm).apply(false)
         alias(kotlinAndroid).apply(false)
         alias(androidLibrary).apply(false)
         alias(kover)
