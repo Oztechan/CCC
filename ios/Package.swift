a98ae859d9a4b93eedbda47b8f235e0be51d83ad
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-03-20 11:50:45 +0100

    [Oztechan/CCC#3237] Add Package.swift for Renovate

---
+++ ios/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CCC",
+    products: [
+        .library(
+            name: "CCC",
+            targets: ["CCC"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            from: "9.6.0"
+        ),
+        .package(
+            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+            from: "9.14.0"
+        ),
+        .package(
+            url: "https://github.com/exyte/PopupView.git",
+            from: "1.3.1"
+        ),
+        .package(
+            url: "https://github.com/matteopuc/swiftui-navigation-stack",
+            from: "1.0.6"
+        )
+    ],
+    targets: [
+        .target(name: "CCC")
+    ]
+)
