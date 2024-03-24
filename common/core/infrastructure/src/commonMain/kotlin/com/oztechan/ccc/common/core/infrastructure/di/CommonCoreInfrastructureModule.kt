8b47958f9a1cebff9891f0f3901799111ac6dda4
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-03-24 22:08:44 +0100

    [Oztechan/CCC#3260] Use IO dispatcher for iOS and remove platform specific dispatchers (#3261)

--- common/core/infrastructure/src/commonMain/kotlin/com/oztechan/ccc/common/core/infrastructure/di/CommonCoreInfrastructureModule.kt
+++ common/core/infrastructure/src/commonMain/kotlin/com/oztechan/ccc/common/core/infrastructure/di/CommonCoreInfrastructureModule.kt
@@ -1,6 +1,13 @@
 package com.oztechan.ccc.common.core.infrastructure.di
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.IO
 import org.koin.core.module.Module
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
 // names
 const val DISPATCHER_IO = "DISPATCHER_IO"
@@ -8,4 +15,11 @@ const val DISPATCHER_MAIN = "DISPATCHER_MAIN"
 const val DISPATCHER_UNCONFINED = "DISPATCHER_UNCONFINED"
 const val DISPATCHER_DEFAULT = "DISPATCHER_DEFAULT"
 
-expect val commonCoreInfrastructureModule: Module
+@Suppress("OPT_IN_USAGE")
+val commonCoreInfrastructureModule: Module = module {
+    single<CoroutineScope> { GlobalScope }
+    single<CoroutineDispatcher>(named(DISPATCHER_MAIN)) { Dispatchers.Main }
+    single(named(DISPATCHER_IO)) { Dispatchers.IO }
+    single(named(DISPATCHER_UNCONFINED)) { Dispatchers.Unconfined }
+    single(named(DISPATCHER_DEFAULT)) { Dispatchers.Default }
+}
