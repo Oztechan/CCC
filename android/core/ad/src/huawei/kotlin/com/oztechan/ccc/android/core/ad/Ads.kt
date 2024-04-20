93c4a97492178f53e34118282802d561bb381712
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-04-20 23:08:48 +0200

    [Oztechan/CCC#3344] Make initAd extension of Activity (#3345)
    * [Oztechan/CCC#3342] Move Ad initialization into Activity
    
    * [Oztechan/CCC#3342] Move Ad initialization into Activity
    
    * [Oztechan/CCC#3344] Make initAd extension of Activity

--- android/core/ad/src/huawei/kotlin/com/oztechan/ccc/android/core/ad/Ads.kt
+++ android/core/ad/src/huawei/kotlin/com/oztechan/ccc/android/core/ad/Ads.kt
@@ -1,10 +1,10 @@
 package com.oztechan.ccc.android.core.ad
 
-import android.content.Context
+import android.app.Activity
 import co.touchlab.kermit.Logger
 import com.huawei.hms.ads.HwAds
 
-fun Context.initAds() {
+fun Activity.initAds() {
     Logger.v { "Ads initAds" }
     HwAds.init(this)
     HwAds.setVideoVolume(0f)
