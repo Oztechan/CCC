b0346350165ab7cdeb1884acc9186f0752f578b1
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-04-20 12:42:11 +0200

    [Oztechan/CCC#3342] Move Ad initialization into Activity (#3343)
    * [Oztechan/CCC#3342] Move Ad initialization into Activity
    
    * [Oztechan/CCC#3342] Move Ad initialization into Activity

--- android/core/ad/src/google/kotlin/com/oztechan/ccc/android/core/ad/AdManagerImpl.kt
+++ android/core/ad/src/google/kotlin/com/oztechan/ccc/android/core/ad/AdManagerImpl.kt
@@ -8,7 +8,6 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.gms.ads.rewarded.RewardedAd
@@ -22,8 +21,6 @@ internal class AdManagerImpl : AdManager {
 
     init {
         Logger.v { "AdManagerImpl init" }
-        MobileAds.setAppVolume(0.0f)
-        MobileAds.setAppMuted(true)
     }
 
     override fun getBannerAd(
--- android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/main/MainActivity.kt
+++ android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/main/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.lifecycleScope
 import co.touchlab.kermit.Logger
 import com.github.submob.basemob.activity.BaseActivity
 import com.oztechan.ccc.android.core.ad.AdManager
+import com.oztechan.ccc.android.core.ad.initAds
 import com.oztechan.ccc.android.ui.mobile.BuildConfig
 import com.oztechan.ccc.android.ui.mobile.R
 import com.oztechan.ccc.android.ui.mobile.util.getThemeMode
@@ -46,6 +47,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         Logger.i { "MainActivity onCreate" }
         setContentView(R.layout.activity_main)
+        initAds()
         observeStates()
         observeEffects()
     }
