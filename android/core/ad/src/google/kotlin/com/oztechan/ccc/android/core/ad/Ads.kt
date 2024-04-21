5e601f39ee51757527db5ee6f6e4a7bd35a1929a
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-04-21 12:32:17 +0200

    [Oztechan/CCC#3352] Add Google UMP and CMP logic for Android (#3353)
    * [Oztechan/CCC#3352] Add Google UMP and CMP logic for Android
    
    * [Oztechan/CCC#3352] Add Google UMP and CMP logic for Android

--- android/core/ad/src/google/kotlin/com/oztechan/ccc/android/core/ad/Ads.kt
+++ android/core/ad/src/google/kotlin/com/oztechan/ccc/android/core/ad/Ads.kt
@@ -3,9 +3,54 @@ package com.oztechan.ccc.android.core.ad
 import android.app.Activity
 import co.touchlab.kermit.Logger
 import com.google.android.gms.ads.MobileAds
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.UserMessagingPlatform
+import java.util.concurrent.atomic.AtomicBoolean
+
+// Use an atomic boolean to initialize the Google Mobile Ads SDK and load ads once.
+private val isMobileAdsInitializeCalled = AtomicBoolean(false)
 
 fun Activity.initAds() {
     Logger.v { "Ads initAds" }
+
+    val consentInformation: ConsentInformation = UserMessagingPlatform.getConsentInformation(this)
+    val params = ConsentRequestParameters.Builder().build()
+
+    consentInformation.requestConsentInfoUpdate(
+        this,
+        params,
+        {
+            UserMessagingPlatform.loadAndShowConsentFormIfRequired(this) { loadAndShowError ->
+                if (loadAndShowError != null) {
+                    Logger.e { "Consent gathering failed: ${loadAndShowError.errorCode}: ${loadAndShowError.message}" }
+                }
+
+                // Consent has been gathered.
+                if (consentInformation.canRequestAds()) {
+                    initializeMobileAdsSdk()
+                }
+            }
+        },
+        { Logger.e { "Consent gathering failed: ${it.errorCode}: ${it.message}" } }
+    )
+
+    // Check if you can initialize the Google Mobile Ads SDK in parallel
+    // while checking for new consent information. Consent obtained in
+    // the previous session can be used to request ads.
+    if (consentInformation.canRequestAds()) {
+        initializeMobileAdsSdk()
+    }
+}
+
+private fun Activity.initializeMobileAdsSdk() {
+    Logger.v { "Ads initializeMobileAdsSdk" }
+
+    if (isMobileAdsInitializeCalled.getAndSet(true)) {
+        Logger.v { "Ads initializeMobileAdsSdk is not called, already called" }
+        return
+    }
+
     MobileAds.initialize(this)
     MobileAds.setAppVolume(0.0f)
     MobileAds.setAppMuted(true)
