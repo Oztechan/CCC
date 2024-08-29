7a87c2ed514f4f9ac6f1718cc4e1bd02723e0dd2
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-08-29 17:07:39 +0200

    [Oztechan/CCC#1457] Update dependency exyte/PopupView to v3

--- ios/Package.swift
+++ ios/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/exyte/PopupView.git",
-            from: "3.0.7"
+            from: "3.0.3"
         ),
         .package(
             url: "https://github.com/matteopuc/swiftui-navigation-stack",
