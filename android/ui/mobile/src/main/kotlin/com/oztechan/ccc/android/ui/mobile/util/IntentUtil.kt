32d280145a25fafd569a474feaa05cd5fe86c4c7
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-09-16 12:57:07 +0200

    [Oztechan/CCC#3954] Update resolveAndStartIntent with more error prone approach

--- android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/util/IntentUtil.kt
+++ android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/util/IntentUtil.kt
@@ -1,13 +1,22 @@
 package com.oztechan.ccc.android.ui.mobile.util
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import co.touchlab.kermit.Logger
 
+@Suppress("TooGenericExceptionCaught")
 fun Context.resolveAndStartIntent(intent: Intent) {
-    intent.resolveActivity(packageManager)?.let {
-        startActivity(intent)
-    } ?: Exception("No activity found to handle the intent: $intent").let {
+    try {
+        intent.resolveActivity(packageManager)?.let {
+            startActivity(intent)
+            null
+        } ?: Exception("No activity found to handle the intent: $intent")
+    } catch (e: ActivityNotFoundException) {
+        Exception("Unable to open link", e)
+    } catch (e: Exception) {
+        Exception("An error occurred", e)
+    }.let {
         Logger.e(it) { it.message.orEmpty() }
     }
 }
