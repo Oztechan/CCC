2efbff751343323659176dcbcc358be1c7fcaf46
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-09-29 11:50:23 +0200

    [Oztechan/CCC#3995] Use injections inside the routes

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/ErrorRoute.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/ErrorRoute.kt
@@ -5,6 +5,7 @@
 package com.oztechan.ccc.backend.app.routes
 
 import co.touchlab.kermit.Logger
+import com.oztechan.ccc.common.core.infrastructure.di.DISPATCHER_IO
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
@@ -14,11 +15,15 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import org.koin.core.qualifier.named
+import org.koin.ktor.ext.inject
 
 private const val PATH_ERROR = "/error"
 private const val ERROR_HTML = "error.html"
 
-internal fun Route.error(ioDispatcher: CoroutineDispatcher) {
+internal fun Route.error() {
+    val ioDispatcher: CoroutineDispatcher by inject(named(DISPATCHER_IO))
+
     get(PATH_ERROR) {
         Logger.v { "GET Request $PATH_ERROR" }
 
