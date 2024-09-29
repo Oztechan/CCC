daf57dc96ad043d76812619af0a2223ac1b2e782
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2024-09-29 11:41:52 +0200

    [Oztechan/CCC#3996] Move threading into Routes (#3998)
    * [Oztechan/CCC#3996] Move threading into Routes
    
    * [Oztechan/CCC#3996] Move threading into Routes

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/RootRoute.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/RootRoute.kt
@@ -12,17 +12,21 @@ import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 private const val PATH_ROOT = "/"
 private const val INDEX_HTML = "index.html"
 
-internal fun Route.root() {
+internal fun Route.root(ioDispatcher: CoroutineDispatcher) {
     get(PATH_ROOT) {
         Logger.v { "GET Request $PATH_ROOT" }
 
-        javaClass.classLoader?.getResource(INDEX_HTML)?.readText()?.let { resource ->
+        withContext(ioDispatcher) {
+            javaClass.classLoader?.getResource(INDEX_HTML)?.readText()
+        }?.let { rootPage ->
             call.respondText(
-                text = resource,
+                text = rootPage,
                 contentType = ContentType.Text.Html,
                 status = HttpStatusCode.OK
             )
