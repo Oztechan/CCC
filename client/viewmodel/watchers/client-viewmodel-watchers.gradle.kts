8bd2a686b7b3bc786807e0868c62b1161a95265b
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-03-02 18:09:15 +0100

    [Oztechan/CCC#4235] Enable Desugaring in the modules that using remote config directly or indirectly

--- client/viewmodel/watchers/client-viewmodel-watchers.gradle.kts
+++ client/viewmodel/watchers/client-viewmodel-watchers.gradle.kts
@@ -60,6 +60,12 @@ android {
         compileOptions {
             sourceCompatibility = JAVA_VERSION
             targetCompatibility = JAVA_VERSION
+            // needed for gitlive remoteconfig, we have it in app module though
+            isCoreLibraryDesugaringEnabled = true
+        }
+
+        dependencies {
+            coreLibraryDesugaring(libs.android.androidDesugaring)
         }
     }
 }
