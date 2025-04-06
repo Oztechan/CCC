1bee2ced3da173400c80cbb7b125e2286e24bfc2
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-04-06 19:22:31 +0200

    [Oztechan/CCC#1457] Update ktor monorepo to v3

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/Application.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/Application.kt
@@ -5,21 +5,9 @@
 package com.oztechan.ccc.backend.app
 
 import co.touchlab.kermit.Logger
-import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.EngineMain
-import io.ktor.server.netty.Netty
-
-private const val RUNNING_LIMIT = 30
 
 fun main(args: Array<String>) {
     Logger.i { "ApplicationKt main" }
-
     EngineMain.main(args)
-
-    embeddedServer(
-        factory = Netty,
-        configure = {
-            runningLimit = RUNNING_LIMIT
-        }
-    ).start(wait = true)
 }
