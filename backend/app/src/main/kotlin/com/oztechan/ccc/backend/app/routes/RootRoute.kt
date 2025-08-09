3d3755667fe341dc86ca333a2e1d9a276c3e67b3
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-08-09 23:34:11 +0200

    [Oztechan/CCC#4404] Remove dispatchers and scopes from backend Routes (#4405)

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/RootRoute.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/routes/RootRoute.kt
@@ -5,36 +5,25 @@
 package com.oztechan.ccc.backend.app.routes
 
 import co.touchlab.kermit.Logger
-import com.oztechan.ccc.common.core.infrastructure.di.DISPATCHER_IO
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
-import org.koin.core.qualifier.named
-import org.koin.ktor.ext.inject
 
 private const val PATH_ROOT = "/"
 private const val INDEX_HTML = "index.html"
 
-internal fun Route.root() {
-    val ioDispatcher: CoroutineDispatcher by inject(named(DISPATCHER_IO))
+internal fun Route.root() = get(PATH_ROOT) {
+    Logger.v { "GET Request $PATH_ROOT" }
 
-    get(PATH_ROOT) {
-        Logger.v { "GET Request $PATH_ROOT" }
-
-        withContext(ioDispatcher) {
-            javaClass.classLoader?.getResource(INDEX_HTML)?.readText()
-        }?.let { rootPage ->
+    javaClass.classLoader?.getResource(INDEX_HTML)?.readText()
+        ?.let { rootPage ->
             call.respondText(
                 text = rootPage,
                 contentType = ContentType.Text.Html,
                 status = HttpStatusCode.OK
             )
         } ?: call.respond(HttpStatusCode.NotFound)
-    }
 }
