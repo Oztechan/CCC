e0f5cb9758e077c78d22b00e939196ba102e8425
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-08-10 11:41:43 +0200

    [Oztechan/CCC#4409] Move to programmatic configuration

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/Application.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/Application.kt
@@ -4,6 +4,38 @@
 
 package com.oztechan.ccc.backend.app
 
-import io.ktor.server.netty.EngineMain
+import co.touchlab.kermit.Logger
+import com.oztechan.ccc.backend.app.module.apiModule
+import com.oztechan.ccc.backend.app.module.koinModule
+import com.oztechan.ccc.backend.app.module.ktorModule
+import com.oztechan.ccc.backend.app.module.loggerModule
+import com.oztechan.ccc.backend.app.module.syncModule
+import io.ktor.server.application.serverConfig
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) = EngineMain.main(args)
+fun main() {
+    Logger.i { "ApplicationKt main" }
+
+    val appProperties = serverConfig {
+        module {
+            loggerModule()
+            koinModule()
+            ktorModule()
+            apiModule()
+            syncModule()
+        }
+    }
+
+    embeddedServer(
+        Netty,
+        appProperties,
+    ) {
+        connector {
+            host = "127.0.0.1"
+            port = 8080
+        }
+    }.start(true)
+}
+
