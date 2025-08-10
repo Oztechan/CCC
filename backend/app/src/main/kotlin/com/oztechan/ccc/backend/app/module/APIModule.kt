7e9dcc252494b0e056eb2ccccd7b59d4615c866d
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2025-08-10 11:40:50 +0200

    [Oztechan/CCC#4407] Pass APIController as constructor parameter since koin no longer supports Route injection (#4408)

--- backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/module/APIModule.kt
+++ backend/app/src/main/kotlin/com/oztechan/ccc/backend/app/module/APIModule.kt
@@ -5,23 +5,20 @@ import com.oztechan.ccc.backend.app.routes.currency
 import com.oztechan.ccc.backend.app.routes.error
 import com.oztechan.ccc.backend.app.routes.root
 import com.oztechan.ccc.backend.app.routes.version
-import io.ktor.serialization.kotlinx.json.json
+import com.oztechan.ccc.backend.controller.api.APIController
 import io.ktor.server.application.Application
-import io.ktor.server.application.install
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
+import org.koin.ktor.ext.inject
 
 @Suppress("unused")
 internal fun Application.apiModule() {
     Logger.v { "APIModuleKt Application.apiModule" }
 
-    install(ContentNegotiation) {
-        json()
-    }
+    val apiController: APIController by inject()
 
     routing {
         root()
-        currency()
+        currency(apiController)
         version()
         error()
     }
