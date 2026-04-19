3d38d25c8f6ed6eab97c380170fd220334c5c0ec
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2026-04-19 22:57:53 +0200

    [Oztechan/CCC#4695] Update COMPILE_SDK_VERSION to 36

--- android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/main/ComposeMainActivity.kt
+++ android/ui/mobile/src/main/kotlin/com/oztechan/ccc/android/ui/mobile/content/main/ComposeMainActivity.kt
@@ -3,6 +3,7 @@ package com.oztechan.ccc.android.ui.mobile.content.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,6 +17,7 @@ import com.oztechan.ccc.android.ui.mobile.theme.AppTheme
 
 class ComposeMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
