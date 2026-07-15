00e47ccaf8559eac08fec2d43e08cda0845f2f11
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2026-07-15 23:56:41 +0200

    [Oztechan/CCC#4796] Make TerminationException extend CancellationException (#4797)

--- common/core/network/src/commonMain/kotlin/com/oztechan/ccc/common/core/network/error/TerminationException.kt
+++ common/core/network/src/commonMain/kotlin/com/oztechan/ccc/common/core/network/error/TerminationException.kt
@@ -1,3 +1,7 @@
 package com.oztechan.ccc.common.core.network.error
 
-internal class TerminationException(cause: Throwable) : Throwable(cause)
+import kotlinx.coroutines.CancellationException
+
+// Maps coroutine cancellation into the project's error taxonomy. Extends CancellationException (not
+// Throwable) so structured concurrency still recognises it as cancellation and unwinds cleanly.
+internal class TerminationException(cause: Throwable) : CancellationException(cause.message)
